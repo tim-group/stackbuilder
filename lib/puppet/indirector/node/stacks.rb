34060318ea3b72a379954ad249b1a9d59d7df421
Richard Pearce <richard.pearce@timgroup.com>
2012-11-29 16:36:53 +0000

    dellis: Did stuff

--- lib/puppet/indirector/node/stacks.rb
+++ lib/puppet/indirector/node/stacks.rb
@@ -2,19 +2,28 @@ require 'stacks/environment'
 require 'puppet/node'
 require 'puppet/indirector/plain'
 
-class Puppet::Node::Stackbuilder < Puppet::Indirector::Plain
+class Puppet::Node::Stacks < Puppet::Indirector::Plain
   desc "generates the necessary wiring for all nodes in a stack."
 
   def find(request)
+    extend Stacks
     node = super
     node.fact_merge
 
-    env = env "dev", :domain=>"dev.net.local" do
-      virtualservice "refapp"
+    env = env "dev" do
+      stack "ref" do
+        loadbalancer "lb"
+        virtualservice "refapp"
+      end
     end
+
     env.generate()
-    member = env.collapse_registries[node.name]
-    node.classes = member.to_enc[:enc][:classes]
+    member = env.collapse_registries[node.parameters['hostname']]
+
+    unless  member.nil?
+      node.classes = member.to_enc[:enc][:classes]
+    end
+
     node
   end
 end
