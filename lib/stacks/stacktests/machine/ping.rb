5a668e3dffb5ba89d226cc64176dbfe6832e813e
David Ellis <david.ellis@timgroup.com>
2013-01-15 13:02:38 +0000

    tanderson/dellis: timing out ping quicker

--- lib/stacks/stacktests/machine/ping.rb
+++ lib/stacks/stacktests/machine/ping.rb
@@ -1,6 +1,6 @@
 shared_examples_for "ping" do |machine|
   it "responds to ping on its management network" do
-    output = `/bin/ping -c 1 -n #{machine.fqdn} 2>&1`
+    output = `/bin/ping -c 1 -W 1 -n #{machine.fqdn} 2>&1`
     $?.should eql(0), output
   end
 end
