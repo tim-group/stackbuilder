7913b86488a5d92e8a39b407a3fd73631806fdf5
David Ellis <david.ellis@timgroup.com>
2013-01-15 11:47:19 +0000

    dellis: tidying: setting build directory, removing putz and correcting gemspec

--- lib/stacks/rake/tasks.rb
+++ lib/stacks/rake/tasks.rb
@@ -17,6 +17,7 @@ bind_to(environment_name)
 RSpec::Core::Runner.disable_autorun!
 config = RSpec.configuration
 config.color_enabled = true
+ENV['CI_REPORTS'] = 'build/spec/reports/'
 
 namespace :sbx do
   accept do |machine_def|
@@ -26,38 +27,21 @@ namespace :sbx do
       task :launch do
         pp machine_def.machines
       end
-    end
-  end
 
-  # we should fuse this loop with the above one - one call to accept
-  # should move the test definition code out into a method to make that clean
-  # just leave the task blocks in the loop
-  accept do |machine_def|
-    namespace machine_def.name.to_sym do
       desc "test"
       task :test do
         machine_def.accept do |machine_def|
           specpath = File.dirname(__FILE__) + "/../stacktests/#{machine_def.clazz}/*.rb"
-
           describe "#{machine_def.clazz}.#{machine_def.name}" do
             Dir[specpath].each do |file|
               require file
-              puts "required " + file
               test = File.basename(file, '.rb')
               it_behaves_like test, machine_def
-              puts "for #{machine_def.name} added #{test}"
             end
           end
         end
         RSpec::Core::Runner.run(['--format','CI::Reporter::RSpec'],$stderr,$stdout)
       end
-    end
-
-    def self.extended(object)
-      self.children.each do |child|
-        child.define_rspec
-      end
-      tests = self.rspecs
     end
   end
 end
--- stacks.gemspec
+++ stacks.gemspec
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 Gem::Specification.new do |s|
-  s.name = "stack_enc"
+  s.name = "stacks"
   s.version = "0.0.0"
   s.authors = ["TIMGroup Infrastructure Team"]
   s.default_executable = %q{}
