8ffeb5bc81deae4b91f51226003596059870122f
David Ellis <david.ellis@timgroup.com>
2013-01-21 09:35:13 +0000

    dellis: fixing ping

--- lib/stacks/stacktests/machine/ping.rb
+++ lib/stacks/stacktests/machine/ping.rb
@@ -5,12 +5,19 @@ shared_examples_for "ping" do |machine|
 
     raise "cant resolve machine #{machine.fqdn}" if ip.nil? or ip==""
 
+    exitcode = -1
+    output = ""
     10.times do
       output = `/bin/ping -c 1 -W 1 -n #{ip} 2>&1`
-      next unless $?.to_i==0
-      $?.to_i.should eql(0), output
+      unless $?.to_i==0
+        sleep 4
+        next
+      end
+      exitcode = $?
       break
     end
+
+    exitcode.to_i.should eql(0), output
 
   end
 end
