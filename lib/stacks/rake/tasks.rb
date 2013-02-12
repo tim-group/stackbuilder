6d5a24b275eb32ea9d1f51b8a951f568c59790c2
David Ellis <david.ellis@timgroup.com>
2013-02-12 12:23:54 +0000

    dellis/agibbins: run tests as well

--- lib/stacks/rake/tasks.rb
+++ lib/stacks/rake/tasks.rb
@@ -86,7 +86,7 @@ namespace :sbx do
         puts machine_def.to_specs.to_yaml
       end
 
-      task :provision=> [:launch, :puppet_clean, :mping, :puppet_sign, :puppet]
+      task :provision=> [:launch, :puppet_clean, :mping, :puppet_sign, :puppet, :test]
 
       desc "allocate these machines to hosts (but don't actually launch them - this is a dry run)"
       task :allocate do
