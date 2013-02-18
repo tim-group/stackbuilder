6ceacf05e700987401d466b1f9ad1f7e0e351ca0
David Ellis <david.ellis@timgroup.com>
2013-02-18 10:52:01 +0000

    dellis: nrpe tests put back

---
+++ lib/stacks/stacktests/machine/nrpe.rb
@@ -0,0 +1,18 @@
+shared_examples_for "nrpe" do |machine|
+  commands = mco_client("nrpe",:nodes=>[machine.mgmt_fqdn]) do |mco|
+    results = []
+    mco.runallcommands().each do |resp|
+      results << resp[:data][:commands]
+    end
+    results
+  end
+
+  commands[0].each do |command,command_result|
+    it "#{command}" do
+      if (command_result[:exitcode]!=0)
+        pp command_result
+        fail(command_result[:output])
+      end
+    end
+  end
+end
