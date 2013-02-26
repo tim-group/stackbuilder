ea459c4c075bde7cacf9de1fcf4a3baffa7e7724
Gary Richards <gary.richards@youdevise.com>
2013-02-26 16:09:20 +0000

    dellis/grichards: use nettest to curl the vip

--- lib/stacks/stacktests/virtualservice/vip.rb
+++ lib/stacks/stacktests/virtualservice/vip.rb
@@ -1,8 +1,13 @@
 shared_examples_for "vip" do |virtualservice|
 
-  pp virtualservice
+  it "can connect to #{virtualservice.vip_fqdn} on port 8000" do
+    data = mco_client("nettest",:nodes=>["st-nat-001.mgmt.st.net.local"]) do |mco|
+      result = mco.connect(:fqdn=>virtualservice.vip_fqdn, :port=>"8000")[0]
+      raise "error attempting to connect to vip #{virtualservice.vip_fqdn}" if result[:statuscode]!=0
+      result[:data]
+    end
 
-#  commands = mco_client("nettest",:nodes=>[machine.mgmt_fqdn]) do |mco|
- # end
+    data[:connect].should eql("Connected")
+  end
 
 end
