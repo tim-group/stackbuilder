cbd8226a02356fee727b2b6c08a106f3499e0fa0
David Ellis <david.ellis@timgroup.com>
2013-03-05 08:49:09 +0000

    dellis: avoid accidental monkey patch of find method

--- lib/puppet/indirector/node/stacks.rb
+++ lib/puppet/indirector/node/stacks.rb
@@ -5,22 +5,23 @@ class Puppet::Node::Stacks < Puppet::Indirector::Plain
   desc "generates the necessary wiring for all nodes in a stack."
 
   def initialize
-    extend Stacks::DSL
+    @stacks_inventory = Object.new
+    @stacks_inventory.extend Stacks::DSL
     dirs = ['.','/etc/stacks/']
     dirs.each do |dir|
       file = "#{dir}/stack.rb"
       if File.exist? file
         config = IO.read file
-        instance_eval(config, file)
+        @stacks_inventory.instance_eval(config, file)
       end
     end
-    bind
+    @stacks_inventory.bind
   end
 
   def find(request)
     node = super
     node.fact_merge
-    machine = find node.parameters['fqdn']
+    machine = @stacks_inventory.find node.parameters['fqdn']
     unless machine.nil?
       node.classes = machine.to_enc
     end
