c99de0dc128768e09bada69bd9b79de778a0f372
Richard Pearce <richard.pearce@timgroup.com>
2013-03-05 15:25:29 +0000

    rpearce/grichards/tanderson: Fixing hard coded networks for standalone servers

--- lib/stacks/standalone_server.rb
+++ lib/stacks/standalone_server.rb
@@ -4,10 +4,10 @@ require 'stacks/machine_def'
 class Stacks::StandaloneServer < Stacks::MachineDef
   attr_reader :environment
 
-  def initialize(base_hostname, location, &block)
+  def initialize(base_hostname, location, networks=[:mgmt, :prod], &block)
     @base_hostname = base_hostname
     @location = location
-    @networks = [:mgmt, :prod]
+    @networks = networks
     block.call unless block.nil?
   end
 
