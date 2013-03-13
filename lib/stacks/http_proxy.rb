6b950477a7f149880456de0c40878871db834242
dellis <david.ellis@timgroup.com>
2013-03-13 08:47:35 +0000

    dellis: add checking so that there is only one vhost for a particular fqdn

--- lib/stacks/http_proxy.rb
+++ lib/stacks/http_proxy.rb
@@ -7,31 +7,64 @@ class Stacks::HttpProxy < Stacks::MachineDef
     super(base_hostname)
     @virtualservice = virtualservice
     @downstream_services = []
+    @proxy_vhosts_lookup = {}
+    @proxy_vhosts = []
   end
 
   def bind_to(environment)
     super(environment)
   end
 
-  def add(service)
-    @downstream_services << service
+  class Stacks::ProxyVHost
+    attr_reader :aliases
+    attr_reader :vhost_fqdn
+
+    def initialize(vhost_fqdn, service, &block)
+      @vhost_fqdn = vhost_fqdn
+      @service = service
+      @aliases = []
+      self.instance_eval &block
+    end
+
+    def add_alias(alias_fqdn)
+      @aliases << alias_fqdn
+    end
+  end
+
+  def add(service, &config_block)
+    @proxy_vhosts << @proxy_vhosts_lookup[service] = Stacks::ProxyVHost.new(virtualservice.vip_front_fqdn, service, &config_block)
   end
 
   def downstream_services
     services = []
+
+    vhost_map = @proxy_vhosts_lookup.values.group_by do |proxy_vhost|
+      proxy_vhost.vhost_fqdn
+    end
+
+    duplicates = Hash[vhost_map.select do |key, values|
+      values.size>1
+    end]
+
+    raise "duplicate keys found #{duplicates.keys.inspect}" unless duplicates.size==0
+
     environment.accept do |machine_def|
       if machine_def.kind_of? Stacks::VirtualService
-        services << machine_def if @downstream_services.include?(machine_def.name)
+        if @proxy_vhosts_lookup.include?(machine_def.name)
+          vhost = @proxy_vhosts_lookup[machine_def.name]
+          services << [virtualservice.vip_front_fqdn, {
+            'aliases' => vhost.aliases,
+            'balancer_members' => [machine_def.vip_fqdn]
+          }]
+        end
       end
     end
+
     return services
   end
 
   def to_enc
-    service_resources = Hash[downstream_services().map do |service|
-      [virtualservice.vip_front_fqdn,[service.vip_fqdn]]
-    end]
-
+    service_resources = Hash[downstream_services()]
     {
       'role::httpproxy' => service_resources
     }
--- spec/stacks/httpproxy_spec.rb
+++ spec/stacks/httpproxy_spec.rb
@@ -7,6 +7,36 @@ describe Stacks::HttpProxy do
 
   end
 
+  it 'blows us if we attempt to use make two vhosts with the same name' do
+    proxy_virtualservice = Stacks::VirtualService.new("proxy")
+    app_virtualservice = Stacks::VirtualService.new("app")
+    appserver = Stacks::Server.new(app_virtualservice, "001")
+    proxyserver = Stacks::HttpProxy.new("proxy-001", proxy_virtualservice)
+    env = Stacks::Environment.new("env", {:primary_site=>"st"}, {})
+
+    proxy_virtualservice.bind_to(env)
+    app_virtualservice.bind_to(env)
+    appserver.bind_to(env)
+
+    proxyserver.add("app") do
+      add_alias "example.timgroup.com"
+    end
+
+    proxyserver.add("app2") do
+      add_alias "example.timgroup.com"
+    end
+
+    proxyserver.bind_to(env)
+
+    env.instance_eval do
+      @machine_def_containers["app"] = app_virtualservice
+      @machine_def_containers["proxy"] = proxy_virtualservice
+    end
+
+    expect {proxyserver.to_enc}.to raise_error
+  end
+
+
   it 'produces an enc references the downstream virtual service it is proxying' do
     proxy_virtualservice = Stacks::VirtualService.new("proxy")
     app_virtualservice = Stacks::VirtualService.new("app")
@@ -17,7 +47,11 @@ describe Stacks::HttpProxy do
     proxy_virtualservice.bind_to(env)
     app_virtualservice.bind_to(env)
     appserver.bind_to(env)
-    proxyserver.add("app")
+
+    proxyserver.add("app") do
+      add_alias "example.timgroup.com"
+    end
+
     proxyserver.bind_to(env)
 
     env.instance_eval do
@@ -26,11 +60,12 @@ describe Stacks::HttpProxy do
     end
 
     proxyserver.to_enc.should eql(
-      {'role::httpproxy'=>
-        {
-          'env-proxy-vip.front.st.net.local' => ["env-app-vip.st.net.local"]
+      {'role::httpproxy'=> {
+          'env-proxy-vip.front.st.net.local' => {
+            'balancer_members' => ["env-app-vip.st.net.local"],
+            'aliases' => ['example.timgroup.com']
+          }
         }
-      }
-    )
+      })
   end
 end
