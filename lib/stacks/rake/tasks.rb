c3f828baa2c7862373cf1067686a8134239321b5
Tomas Doran <tomas.doran@timgroup.com>
2013-03-19 13:32:09 +0000

    Does this cause vip allocation to happen?

--- lib/stacks/rake/tasks.rb
+++ lib/stacks/rake/tasks.rb
@@ -100,7 +100,7 @@ namespace :sbx do
         end
       end
 
-      task :provision=> ['launch', 'puppet:sign', 'puppet:wait']
+      task :provision=> ['allocate_vips', 'launch', 'puppet:sign', 'puppet:wait']
 
       desc "allocate these machines to hosts (but don't actually launch them - this is a dry run)"
       sbtask :allocate do
