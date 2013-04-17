222ff50ee9378a73472d0c447ea8f6e0df086235
Tomas Doran <tomas.doran@timgroup.com>
2013-04-17 10:38:43 +0100

    Remember to remove this code

--- lib/compute/controller.rb
+++ lib/compute/controller.rb
@@ -136,17 +136,3 @@ class Compute::Controller
 
 end
 
-require 'socket'
-require 'ipaddr'
-
-class Support::DNS
-  # returns nil if lookup fails, but propagates any errors in formatting
-  def gethostbyname(hostname)
-    begin
-      addrinfo = Socket.gethostbyname(hostname)
-    rescue
-      return nil
-    end
-    return IPAddr.ntop(addrinfo[3])
-  end
-end
