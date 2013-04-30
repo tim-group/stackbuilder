7780a54186b8dcae99f2a020e47fb030daa3d272
Tomas Doran <tomas.doran@timgroup.com>
2013-04-30 11:57:20 +0100

    Move random classes into places that match their class name

---
+++ lib/stacks/proxy_vhost.rb
@@ -0,0 +1,25 @@
+require 'stacks/namespace'
+require 'uri'
+
+class Stacks::ProxyVHost
+  attr_reader :aliases
+  attr_reader :vhost_fqdn
+  attr_reader :redirects
+
+  def initialize(vhost_fqdn, service, &block)
+    @vhost_fqdn = vhost_fqdn
+    @service = service
+    @aliases = []
+    @redirects = []
+    self.instance_eval &block
+  end
+
+  def with_alias(alias_fqdn)
+    @aliases << alias_fqdn
+  end
+
+  def with_redirect(redirect_fqdn)
+    @redirects << redirect_fqdn
+  end
+end
+
--- lib/stacks/virtual_proxy_service.rb
+++ lib/stacks/virtual_proxy_service.rb
@@ -2,29 +2,8 @@ require 'stacks/namespace'
 require 'stacks/machine_def_container'
 require 'stacks/app_server'
 require 'stacks/nat'
+require 'stacks/proxy_vhost'
 require 'uri'
-
-class Stacks::ProxyVHost
-  attr_reader :aliases
-  attr_reader :vhost_fqdn
-  attr_reader :redirects
-
-  def initialize(vhost_fqdn, service, &block)
-    @vhost_fqdn = vhost_fqdn
-    @service = service
-    @aliases = []
-    @redirects = []
-    self.instance_eval &block
-  end
-
-  def with_alias(alias_fqdn)
-    @aliases << alias_fqdn
-  end
-
-  def with_redirect(redirect_fqdn)
-    @redirects << redirect_fqdn
-  end
-end
 
 class Stacks::VirtualProxyService < Stacks::VirtualService
   attr_reader :proxy_vhosts
