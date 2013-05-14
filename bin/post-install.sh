bbb4a6d74587392263e5390d0966ba307c04e10a
Tomas Doran <tomas.doran@timgroup.com>
2013-05-14 14:28:22 +0100

    Try to not restart puppetmasters till they're idle

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -2,6 +2,12 @@
 if [ -f "/etc/init.d/apache2-puppetmaster" ]; then
   /etc/init.d/apache2-puppetmaster status >/dev/null 2>&1
   if [ $? -eq "0" ]; then
+    x=$(netstat -nalp | grep 8140 | grep ESTABLISHED | wc -l)
+    while [ $x -ge 0 ]
+    do
+      echo "Waiting for puppetmaster connections to drop to 0, currently $x"
+      x=$(netstat -nalp | grep 8140 | grep ESTABLISHED | wc -l)
+    done
     /etc/init.d/apache2-puppetmaster restart
   fi
 fi
