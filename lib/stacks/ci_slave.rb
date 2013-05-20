3b3bf0d95dd738ea5216b0a6cf22df2078267f88
David Ellis <david.ellis@timgroup.com>
2013-05-20 13:25:43 +0100

    dellis: converting nat and ci to new way

--- lib/stacks/ci_slave.rb
+++ lib/stacks/ci_slave.rb
@@ -2,7 +2,12 @@ require 'stacks/namespace'
 require 'stacks/machine_def'
 
 class Stacks::CiSlave < Stacks::MachineDef
-  def bind_to(environment)
+  def initialize(server_group, index, &block)
+    super(server_group.name + "-" + index, [:mgmt, :prod, :front])
+    self
+  end
+
+ def bind_to(environment)
     super(environment)
   end
 
