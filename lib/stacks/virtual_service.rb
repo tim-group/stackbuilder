ebff898b2a78703519963d7ef9441405eb5226a1
David Ellis <david.ellis@timgroup.com>
2013-05-20 11:59:55 +0100

    dellis: tidyup

--- lib/stacks/virtual_service.rb
+++ lib/stacks/virtual_service.rb
@@ -4,36 +4,6 @@ require 'stacks/app_server'
 require 'stacks/nat'
 require 'uri'
 
-class Stacks::MachineSet
-  attr_accessor :type
-  attr_accessor :name
-  attr_accessor :fabric
-  attr_accessor :instances
-  attr_accessor :ports
-  attr_accessor :groups
-
-  include Stacks::MachineDefContainer
-
-  def initialize(name, &config_block)
-    @name = name
-    @groups = ['blue']
-    @definitions = {}
-    @bind_steps = []
-    @instances = 2
-    @config_block = config_block
-  end
-
-  def on_bind(&block)
-    @bind_steps << block
-  end
-
-  def bind_to(environment)
-    @bind_steps.each do |step|
-      step.call(self, environment)
-    end
-  end
-end
-
 module Stacks::AbstractVirtualService
 end
 
