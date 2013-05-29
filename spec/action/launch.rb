30259c7d33db221f194474a3f1edccb41c2c52bc
David Ellis <david.ellis@timgroup.com>
2013-05-29 16:55:01 +0100

    dellis: launch actions

---
+++ spec/action/launch.rb
@@ -0,0 +1,81 @@
+
+describe 'launch' do
+  class HostRepository
+    attr_accessor :machine_repo
+
+    def find_current
+      result = audit()
+      result.each.each do |vm|
+        machine_repo.find(vm)
+      end
+      host = Host.new(:allocated_machines=>nil, :policies=>nil, :preferences=>nil)
+    end
+  end
+
+
+  class Host
+    attr_accessor :allocated_machines
+    attr_accessor :provisionally_allocated_machines
+
+    def machines
+      # merge allocated and provisionally_allocated
+    end
+
+    def provisionally_allocate(machine)
+    end
+
+    def can_allocate(machine)
+      policies.check(machine)
+      # exclude if asking for too much disk
+      # exclude if asking for too much ram
+      # exclude if already contains a machine in this host group
+    end
+
+    def utility(machine)
+      preferences.rate(machine)
+    end
+  end
+
+  class Hosts
+    attr_accessor :hosts
+
+    private
+    def find_suitable_host_for(machine)
+      candidate_hosts = hosts.reject do |host|
+        !host.can_allocate(host)
+      end.order_by utility
+      candidate_hosts[0]
+    end
+
+    def allocate(machines, hosts)
+      unallocated_machines = hosts.remove_from_list_if_allocated(machines)
+
+      unallocated_machines.each do |machine|
+        host = find_suitable_host_for(machine)
+        host.provisionally_allocate(machine)
+    end
+
+    def to_unlaunched_specs
+
+    end
+  end
+
+  action 'launch' do |services, machine_def|
+    hosts = services.host_repo.find_current()
+    hosts.allocate(machine_def.flatten)
+    specs = hosts.to_unlaunched_specs()
+    services.compute_controller.launch(specs)
+  end
+
+  it 'will allocate and launch a bunch of machines' do
+    stack "ref" do
+      virtual_app_service "refapp"
+    end
+
+    actions.services.host_repo = double()
+    actions.services.compute_controller = double()
+
+    run_action "launch", machine_def
+  end
+
+end
