a42f36c09581b6e1892156df82b30ec488ff96b2
David Ellis <david.ellis@timgroup.com>
2013-06-05 10:42:05 +0100

    dellis/wtaj: oops forgot to add file

---
+++ lib/stacks/elasticsearch_node.rb
@@ -0,0 +1,20 @@
+require 'stacks/namespace'
+require 'stacks/machine_def'
+
+class Stacks::ElasticSearchNode < Stacks::MachineDef
+  def initialize(server_group, index, &block)
+    super(server_group.name + "-" + index, [:mgmt])
+    self
+  end
+
+ def bind_to(environment)
+    super(environment)
+  end
+
+  def to_enc
+    {
+      'role::elasticsearch_node' => {}
+    }
+  end
+end
+
