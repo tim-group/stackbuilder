aaaff2c35ff23ed8ecc4a98d9cb354cabd9b825c
David Ellis <david.ellis@timgroup.com>
2013-07-04 11:02:04 +0100

    dellis: put the guard in the right place - fixing ha group issue

--- lib/stacks/hosts/host_policies.rb
+++ lib/stacks/hosts/host_policies.rb
@@ -3,10 +3,10 @@ require 'stacks/hosts/namespace'
 module Stacks::Hosts::HostPolicies
   def self.ha_group()
     Proc.new do |host, machine|
-      if machine.availability_group != nil
+      if machine.respond_to? :availability_group and machine.availability_group != nil
         member_of_group_exists = false
         host.machines.each do |allocated_machine|
-          if machine.respond_to? :availability_group and machine.availability_group == allocated_machine.availability_group
+          if machine.availability_group == allocated_machine.availability_group
             member_of_group_exists = true
           end
         end
