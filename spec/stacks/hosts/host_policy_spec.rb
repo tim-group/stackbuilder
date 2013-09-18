ea7292f2036c17c7d7f13477baaa315d849516b9
Tom Denley <tom.denley@youdevise.com>
2013-09-18 09:19:10 +0100

    tomd: correct tests for do_not_overallocated_ram_policy

--- spec/stacks/hosts/host_policy_spec.rb
+++ spec/stacks/hosts/host_policy_spec.rb
@@ -37,7 +37,7 @@ describe Stacks::Hosts::HostPolicies do
   it 'allows allocation if the availability group is unset' do
     machine = double
     running_machine= double
-    
+
     machine.stub(:availability_group).and_return(nil)
     running_machine.stub(:availability_group).and_return(nil)
 
@@ -47,18 +47,34 @@ describe Stacks::Hosts::HostPolicies do
   end
 
   it 'allows allocations where the host ram is sufficient' do
-    env = test_env_with_refstack
-    machines = env.flatten
-    h1 = Stacks::Hosts::Host.new("h1", :ram => '4194304')
-    h1.allocated_machines << machines[0]
-    Stacks::Hosts::HostPolicies.ha_group().call(h1, machines[1]).should eql(true)
+    candidate_machine = double
+    provisionally_allocated_machine = double
+    existing_machine = double
+
+    candidate_machine.stub(:ram).and_return(100)
+    provisionally_allocated_machine.stub(:ram).and_return(10)
+    existing_machine.stub(:ram).and_return(1)
+
+    h1 = Stacks::Hosts::Host.new("h1", 111)
+    h1.allocated_machines << existing_machine
+    h1.provisionally_allocated_machines << provisionally_allocated_machine
+
+    Stacks::Hosts::HostPolicies.do_not_overallocated_ram_policy().call(h1, candidate_machine).should eql(true)
   end
 
   it 'rejects allocations where the host ram is insufficient' do
-    env = test_env_with_refstack
-    machines = env.flatten
-    h1 = Stacks::Hosts::Host.new("h1", :ram => '4194303')
-    h1.allocated_machines << machines[0]
-    Stacks::Hosts::HostPolicies.ha_group().call(h1, machines[1]).should eql(false)
+    candidate_machine = double
+    provisionally_allocated_machine = double
+    existing_machine = double
+
+    candidate_machine.stub(:ram).and_return(100)
+    provisionally_allocated_machine.stub(:ram).and_return(10)
+    existing_machine.stub(:ram).and_return(1)
+
+    h1 = Stacks::Hosts::Host.new("h1", 110)
+    h1.allocated_machines << existing_machine
+    h1.provisionally_allocated_machines << provisionally_allocated_machine
+
+    Stacks::Hosts::HostPolicies.do_not_overallocated_ram_policy().call(h1, candidate_machine).should eql(false)
   end
 end
