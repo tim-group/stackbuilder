66395e9f7b28e3436d0f4625396223a5e8033881
Tom Denley <tom.denley@youdevise.com>
2013-09-27 14:30:51 +0100

    tomd: restore the logic for ram policy, but leave it returning true

--- lib/stacks/hosts/host_policies.rb
+++ lib/stacks/hosts/host_policies.rb
@@ -19,7 +19,19 @@ module Stacks::Hosts::HostPolicies
 
   def self.do_not_overallocated_ram_policy
     Proc.new do |host, machine|
-      true
+      host_ram = Integer(host.ram)
+      if host_ram > 0
+        allocated_ram = 0
+        host.machines.each do |allocated_machine|
+          allocated_ram = allocated_ram + Integer(allocated_machine.ram)
+        end
+        available_ram = host_ram - allocated_ram
+        available_ram >= Integer(machine.ram)
+        # override to always return true for now
+        true
+      else
+        true
+      end
     end
   end
 
