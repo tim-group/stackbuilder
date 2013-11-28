53ca63b09f48bcd65fa553e1d1d7f7fb821ffbd0
Richard Pearce <richard.pearce@timgroup.com>
2013-11-28 16:03:27 +0000

    rpearce/dellis: Add test for lb group
      * Added availability group to spec for lb's
      * Check for availability group in quantapps

---
+++ spec/lb_spec.rb
@@ -0,0 +1,29 @@
+require 'stacks/test_framework'
+describe_stack 'lb' do
+  given do
+    stack "lb" do
+      loadbalancer  do
+      end
+    end
+
+    env "e1", :primary_site=>"space" do
+      instantiate_stack "lb"
+    end
+  end
+
+  host("e1-lb-002.mgmt.space.net.local") do |host|
+    host.to_spec.should eql({
+      :fabric=>"space",
+      :qualified_hostnames=>{
+      :mgmt=>"e1-lb-002.mgmt.space.net.local",
+      :prod=>"e1-lb-002.space.net.local"},
+      :group=>"e1-lb",
+      :networks=>[:mgmt, :prod],
+      :hostname=>"e1-lb-002",
+      :ram=>"2097152",
+      :domain=>"space.net.local"})
+  end
+
+end
+
+
