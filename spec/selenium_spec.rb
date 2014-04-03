1bd27ac1122dae7eb3da9f594c727007101975d5
Tony Tsui <tony.tsui@timgroup.com>
2014-04-03 10:23:21 +0100

    Tony: clean code by fixing indenting and spacing

--- spec/selenium_spec.rb
+++ spec/selenium_spec.rb
@@ -2,37 +2,37 @@ require 'stacks/test_framework'
 describe_stack 'selenium' do
   given do
     stack "segrid" do
-      segrid :v=>2 do
-        hub :selenium_version=>"2.41.0"
-        winxp "6", :instances=>2,
+      segrid :v => 2 do
+        hub :selenium_version => "2.41.0"
+        winxp "6", :instances => 2,
                    :gold_image=> "file:///var/local/images/dev-sxp-gold.img",
-                   :selenium_version=>"2.41.0"
-        win7 "9", :instances=>2,
-                 :gold_image=> "http://iso.youdevise.com/gold/win7-ie9-gold.img",
-                 :selenium_version=>"2.39.0"
-        ubuntu :instances=>5,
-               :selenium_version=>"2.32.0"
+                   :selenium_version => "2.41.0"
+        win7 "9", :instances => 2,
+                  :gold_image=> "http://iso.youdevise.com/gold/win7-ie9-gold.img",
+                  :selenium_version => "2.39.0"
+        ubuntu :instances => 5,
+               :selenium_version => "2.32.0"
       end
     end
 
     stack "qatestmachines" do
-      segrid :v=>2 do
-        winxp "6", :instances=>10,
+      segrid :v => 2 do
+        winxp "6", :instances => 10,
                    :gold_image=> "file:///var/local/images/dev-sxp-gold.img",
-                   :selenium_version=>"2.32.0"
-        win7 "9", :instances=>10,
+                   :selenium_version => "2.32.0"
+        win7 "9", :instances => 10,
                   :gold_image=> "http://iso.youdevise.com/gold/win7-ie9-gold.img",
-                  :selenium_version=>"2.32.0"
-        ubuntu :instances=>5,
-               :selenium_version=>"2.32.0"
+                  :selenium_version => "2.32.0"
+        ubuntu :instances => 5,
+               :selenium_version => "2.32.0"
       end
     end
 
-    env "e1", :primary_site=>"space" do
+    env "e1", :primary_site => "space" do
       instantiate_stack "segrid"
     end
 
-    env "qa", :primary_site=>"space" do
+    env "qa", :primary_site => "space" do
       instantiate_stack "qatestmachines"
     end
   end
@@ -63,76 +63,75 @@ describe_stack 'selenium' do
 
   host("e1-hub-001.mgmt.space.net.local") do |host|
     host.to_spec.should eql(
-      { :fabric=>"space",
-        :template=>"sehub",
-        :qualified_hostnames => { :mgmt=>"e1-hub-001.mgmt.space.net.local" },
-        :availability_group=>nil,
-        :networks=>[:mgmt],
-        :hostname=>"e1-hub-001",
-        :ram=>"2097152",
-        :domain=>"space.net.local",
-        :nodes=>["e1-browser-001",
-                 "e1-browser-002",
-                 "e1-browser-003",
-                 "e1-browser-004",
-                 "e1-browser-005",
-                 "e1-win7ie9-001",
-                 "e1-win7ie9-002",
-                 "e1-xp6-001",
-                 "e1-xp6-002"],
-        :selenium_version=>"2.41.0"
+      { :fabric => "space",
+        :template => "sehub",
+        :qualified_hostnames => { :mgmt => "e1-hub-001.mgmt.space.net.local" },
+        :availability_group => nil,
+        :networks => [:mgmt],
+        :hostname => "e1-hub-001",
+        :ram => "2097152",
+        :domain => "space.net.local",
+        :nodes => ["e1-browser-001",
+                   "e1-browser-002",
+                   "e1-browser-003",
+                   "e1-browser-004",
+                   "e1-browser-005",
+                   "e1-win7ie9-001",
+                   "e1-win7ie9-002",
+                   "e1-xp6-001",
+                   "e1-xp6-002"],
+        :selenium_version => "2.41.0"
       }
     )
   end
 
   host("e1-xp6-002.mgmt.space.net.local") do |host|
     host.to_spec.should eql(
-      { :fabric=>"space",
-        :template=>"xpboot",
+      { :fabric => "space",
+        :template => "xpboot",
         :kvm_template => 'kvm_no_virtio',
         :selenium_hub_host => 'e1-hub-001.mgmt.space.net.local',
-        :selenium_version=>"2.41.0",
+        :selenium_version => "2.41.0",
         :gold_image_url => 'file:///var/local/images/dev-sxp-gold.img',
         :launch_script => 'start-grid.bat',
         :image_size => "8G",
-        :qualified_hostnames=> { :mgmt=>"e1-xp6-002.mgmt.space.net.local" },
-        :availability_group=>nil,
-        :networks=>[:mgmt],
-        :hostname=>"e1-xp6-002",
-        :ram=>"2097152",
-        :domain=>"space.net.local"
+        :qualified_hostnames=> { :mgmt => "e1-xp6-002.mgmt.space.net.local" },
+        :availability_group => nil,
+        :networks => [:mgmt],
+        :hostname => "e1-xp6-002",
+        :ram => "2097152",
+        :domain => "space.net.local"
       })
   end
 
   host("e1-win7ie9-002.mgmt.space.net.local") do |host|
     host.to_spec.should eql(
-      { :fabric=>"space",
-        :template=>"win7boot",
+      { :fabric => "space",
+        :template => "win7boot",
         :selenium_hub_host => 'e1-hub-001.mgmt.space.net.local',
-        :selenium_version=>"2.39.0",
+        :selenium_version => "2.39.0",
         :gold_image_url => 'http://iso.youdevise.com/gold/win7-ie9-gold.img',
         :image_size => "15G",
-        :qualified_hostnames=> { :mgmt=>"e1-win7ie9-002.mgmt.space.net.local"},
-        :availability_group=>nil,
-        :networks=>[:mgmt],
-        :hostname=>"e1-win7ie9-002",
-        :ram=>"2097152",
-        :domain=>"space.net.local"})
+        :qualified_hostnames=> { :mgmt => "e1-win7ie9-002.mgmt.space.net.local"},
+        :availability_group => nil,
+        :networks => [:mgmt],
+        :hostname => "e1-win7ie9-002",
+        :ram => "2097152",
+        :domain => "space.net.local"})
   end
 
   host("e1-browser-001.mgmt.space.net.local") do |host|
     host.to_spec.should eql({
-      :fabric=>"space",
-      :template=>"senode",
+      :fabric => "space",
+      :template => "senode",
       :selenium_hub_host => 'e1-hub-001.mgmt.space.net.local',
       :selenium_version => '2.32.0',
-      :qualified_hostnames=>
-      {:mgmt=>"e1-browser-001.mgmt.space.net.local"},
-        :availability_group=>nil,
-        :networks=>[:mgmt],
-        :hostname=>"e1-browser-001",
-        :ram=>"2097152",
-        :domain=>"space.net.local"})
+      :qualified_hostnames=> {:mgmt => "e1-browser-001.mgmt.space.net.local"},
+      :availability_group => nil,
+      :networks => [:mgmt],
+      :hostname => "e1-browser-001",
+      :ram => "2097152",
+      :domain => "space.net.local"})
   end
 
 end
