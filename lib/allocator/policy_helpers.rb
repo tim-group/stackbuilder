8a89347f0b15b9f231b5bc271884db6b3d5b8331
Gary Richards <gary+github@dsnine.co.uk>
2014-04-28 09:32:23 +0100

    Revert "Revert "Add an audit_host_machines rake task that queries the :primary_site of a fabric's  compute nodes and displays their lvm and ram usage.""
    This reverts commit e9b08ac1bfe345025ced624ce39b231fc9a8a019.

---
+++ lib/allocator/policy_helpers.rb
@@ -0,0 +1,24 @@
+module StackBuilder::Allocator::PolicyHelpers
+  def self.ram_stats_of(host)
+    host_ram = Integer(host.ram)
+    host_reserve_ram = 2097152 #2 GB
+    if host_ram > 0
+      allocated_ram = 0
+      host.machines.each do |allocated_machine|
+        allocated_ram = allocated_ram + Integer(allocated_machine[:ram])
+      end
+      available_ram = (host_ram - allocated_ram) - host_reserve_ram
+      result = {
+        :host_ram => host_ram,
+        :host_reserve_ram => host_reserve_ram,
+        :allocated_ram => allocated_ram,
+        :available_ram => available_ram,
+      }
+    end
+    result
+  end
+
+  def self.disk_stats_of(host)
+    host.lvm
+  end
+end
