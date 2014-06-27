c71468d1daeeda7cec5c52fc04408c6fce6f4de6
Besam Khidhir <besam.khidhir@timgroup.com>
2014-06-27 11:53:18 +0100

    bkhidhir / replace fmanalyticsapp with analyticsapp in spec test, take 2

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -1,29 +1,29 @@
 require 'stacks/test_framework'
 
-describe_stack 'fmanalytics' do
+describe_stack 'analytics' do
   given do
-    stack "fmanalytics" do
+    stack "analytics" do
       analyticsapp
     end
 
     env "e1", :primary_site=>"space" do
-      instantiate_stack "fmanalytics"
+      instantiate_stack "analytics"
     end
   end
 
-  host("e1-fmanalyticsapp-001.mgmt.space.net.local") do |host|
+  host("e1-analyticsapp-001.mgmt.space.net.local") do |host|
     host.to_enc.should eql({
-      'role::fmanalyticsapp_server' => {
+      'role::analyticsapp_server' => {
     }})
 
     host.to_specs.should eql([
       {:fabric=>"space",
        :qualified_hostnames=>{
-          :mgmt=>"e1-fmanalyticsapp-001.mgmt.space.net.local",
-          :prod=>"e1-fmanalyticsapp-001.space.net.local"},
-        :availability_group=>"e1-fmanalyticsapp",
+          :mgmt=>"e1-analyticsapp-001.mgmt.space.net.local",
+          :prod=>"e1-analyticsapp-001.space.net.local"},
+        :availability_group=>"e1-analyticsapp",
         :networks=>[:mgmt, :prod],
-        :hostname=>"e1-fmanalyticsapp-001",
+        :hostname=>"e1-analyticsapp-001",
         :ram=>"2097152",
         :storage => {'/'.to_sym =>{:type=>"os", :size=>"3G"}},
         :domain=>"space.net.local"}])
