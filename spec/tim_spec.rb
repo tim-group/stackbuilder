8cb2f0c1484a1a8616db892e13fa9d0a74f61fb4
Richard Pearce <richard.pearce@timgroup.com>
2014-06-27 16:08:49 +0100

    rpearce: Removing most of the test cases as they dont test anything. Should not duplicate the generic testing we already have for standalone_appserver

--- spec/tim_spec.rb
+++ spec/tim_spec.rb
@@ -18,54 +18,11 @@ describe_stack 'tim' do
     end
   end
 
-  host("e1-timcyclic-002.mgmt.space.net.local") do |host|
-    host.to_enc.should eql({
-      'role::http_app' => {
-        'port' => "8000",
-        'environment' => 'e1',
-        'application'  => 'TIM',
-        'group' => 'blue',
-        'dependencies' => [],
-        'dependant_instances' => [],
-      }})
-
-    host.to_specs.should eql([
-      {:fabric=>"space",
-       :qualified_hostnames=>{
-          :mgmt=>"e1-timcyclic-002.mgmt.space.net.local",
-          :prod=>"e1-timcyclic-002.space.net.local"},
-        :availability_group=>"e1-timcyclic",
-        :networks=>[:mgmt, :prod],
-        :hostname=>"e1-timcyclic-002",
-        :ram=>"14680064",
-        :vcpus=>"8",
-        :image_size=>'10G',
-        :storage=>{'/'.to_sym => {:type=>"os", :size=>"10G"}},
-        :domain=>"space.net.local"}])
-  end
   host("e1-timcyclic-001.mgmt.space.net.local") do |host|
-    host.to_enc.should eql({
-      'role::http_app' => {
-        'port' => "8000",
-        'environment' => 'e1',
-        'application'  => 'TIM',
-        'group' => 'blue',
-        'dependencies' => [],
-        'dependant_instances' => [],
-      }})
-
-    host.to_specs.should eql([
-      {:fabric=>"space",
-       :qualified_hostnames=>{
-          :mgmt=>"e1-timcyclic-001.mgmt.space.net.local",
-          :prod=>"e1-timcyclic-001.space.net.local"},
-        :availability_group=>"e1-timcyclic",
-        :networks=>[:mgmt, :prod],
-        :hostname=>"e1-timcyclic-001",
-        :ram=>"14680064",
-        :vcpus=>"8",
-        :image_size=>'10G',
-        :storage=>{'/'.to_sym => {:type=>"os", :size=>"10G"}},
-        :domain=>"space.net.local"}])
+     host.should_not eql nil
   end
+  host("e1-timcyclic-002.mgmt.space.net.local") do |host|
+     host.should_not eql nil
+  end
+
 end
