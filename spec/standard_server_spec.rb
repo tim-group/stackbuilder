68f4bbbce4055a329a8cfe6268377d06634e4420
Richard Pearce <richard.pearce@timgroup.com>
2014-06-27 16:29:18 +0100

    rpearce: Make standard server test standard server and rename to make it sensical

---
+++ spec/standard_server_spec.rb
@@ -0,0 +1,17 @@
+require 'stacks/test_framework'
+
+describe_stack 'standard' do
+  given do
+    stack "standard" do
+      standard "mymachine"
+    end
+
+    env "e1", :primary_site=>"space" do
+      instantiate_stack "standard"
+    end
+  end
+
+  host("e1-mymachine-001.mgmt.space.net.local") do |host|
+    host.to_enc.should eql({'server::default_new_mgmt_net_local' => {}})
+  end
+end
