b08d96df1417bd2f791f5764a26fdfe6ee004f9d
Richard Pearce <richard.pearce@timgroup.com>
2014-06-30 14:46:06 +0100

    rpearce: Ease testing maintenance burden. Modify analytics

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -15,18 +15,5 @@ describe_stack 'analytics' do
     host.to_enc.should eql({
       'role::analyticsapp_server' => {
     }})
-
-    host.to_specs.should eql([
-      {:fabric=>"space",
-       :qualified_hostnames=>{
-          :mgmt=>"e1-analyticsapp-001.mgmt.space.net.local",
-          :prod=>"e1-analyticsapp-001.space.net.local"},
-        :availability_group=>"e1-analyticsapp",
-        :networks=>[:mgmt, :prod],
-        :hostname=>"e1-analyticsapp-001",
-        :ram=>"2097152",
-        :storage => {'/'.to_sym =>{:type=>"os", :size=>"3G"}},
-        :domain=>"space.net.local"}])
-
   end
 end
