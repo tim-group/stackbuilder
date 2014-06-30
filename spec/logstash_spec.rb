d7c6824c99cc0b100ffc7eb901bcfa11f9489297
Richard Pearce <richard.pearce@timgroup.com>
2014-06-30 14:35:05 +0100

    rpearce: Ease testing maintenance burden. Modify logstash

--- spec/logstash_spec.rb
+++ spec/logstash_spec.rb
@@ -16,18 +16,5 @@ describe_stack 'logstash' do
       'role::logstash_server' => {
         'environment' => "e1"
       }})
-
-    host.to_specs.should eql([
-      {:fabric=>"space",
-       :qualified_hostnames=>{
-          :mgmt=>"e1-logstash-001.mgmt.space.net.local",
-          :prod=>"e1-logstash-001.space.net.local"},
-        :availability_group=>"e1-logstash",
-        :networks=>[:mgmt, :prod],
-        :hostname=>"e1-logstash-001",
-        :ram=>"2097152",
-        :storage => {'/'.to_sym =>{:type=>"os", :size=>"3G"}},
-        :domain=>"space.net.local"}])
-
   end
 end
