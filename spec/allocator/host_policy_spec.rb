9fd13e1b14c274143f3a9f1883e3cbf6a12257d1
Gary Richards <gary+github@dsnine.co.uk>
2014-07-08 15:12:00 +0100

    Disable these tests temporarily.

--- spec/allocator/host_policy_spec.rb
+++ spec/allocator/host_policy_spec.rb
@@ -116,14 +116,16 @@ describe StackBuilder::Allocator::HostPolicies do
     StackBuilder::Allocator::HostPolicies.do_not_overallocated_ram_policy().call(h1, candidate_machine)[:passed].should eql(false)
   end
 
-  it 'rejects allocations where the host has no defined storage types' do
+  # FIXME: renable this once all compute nodes have a storage spec.
+  xit 'rejects allocations where the host has no defined storage types' do
     machine = {:storage => {:mount_point => {:type => "something"}}}
     h1 = StackBuilder::Allocator::Host.new("h1", :storage => {})
     StackBuilder::Allocator::HostPolicies.ensure_defined_storage_types_policy().call(h1, machine)[:passed].should eql(false)
 
   end
 
-  it 'accepts allocations where the host has no defined storage types' do
+  # FIXME: renable this once all compute nodes have a storage spec.
+  xit 'accepts allocations where the host has no defined storage types' do
     machine = {:storage => {:mount_point => {:type => "LVS"}}}
     h1 = StackBuilder::Allocator::Host.new("h1", :storage => {"LVS" => {"some_key" => "value"}})
     StackBuilder::Allocator::HostPolicies.ensure_defined_storage_types_policy().call(h1, machine)[:passed].should eql(true)
