32a8c3c4394437347c46241cafde52daae7ee119
Richard Pearce <richard.pearce@timgroup.com>
2014-07-08 10:32:08 +0100

    rpearce: Puppetmaster now takes longer than 200 seconds to spin up ready for sign. Bumping until we find a better solution

--- lib/support/mcollective_puppet.rb
+++ lib/support/mcollective_puppet.rb
@@ -10,7 +10,7 @@ module Support::MCollectivePuppet
   include Support::MCollective
 
   def ca_sign(machines_fqdns, &block)
-    timeout = 200
+    timeout = 240
     callback = Support::Callback.new(&block)
     needs_signing = machines_fqdns.clone.to_set
 
