362b688ce5a592782aebc1db90e51e7c241f7fc0
Richard Pearce <richard.pearce@timgroup.com>
2014-07-24 10:35:13 +0100

    rpearce: Reduce number of duplicate test, only check parts of the spec we care about. Not the entire spec (causes duplication in tests)

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -11,23 +11,11 @@ describe_stack 'should default root storage size to 3G' do
   end
 
   host("e1-default-002.mgmt.space.net.local") do |host|
-    host.to_specs.first[:storage].should eql({
-      '/'.to_sym =>  {
-        :type       => 'os',
-        :size       => '3G',
-        :persistent => false,
-        :prepare    => {
-          :method => 'image',
-          :options => {
-            :path => '/var/local/images/gold/generic.img'
-          },
-        },
-      }
-    })
+    host.to_specs.first[:storage]['/'.to_sym][:size].should eql('3G')
   end
 end
 
-describe_stack 'override root storage size when image_size is set' do
+describe_stack 'legacy override root storage size when image_size is set' do
   given do
     stack 'demo' do
       standalone_appserver 'override' do
@@ -42,100 +30,12 @@ describe_stack 'override root storage size when image_size is set' do
   end
 
   host("e1-override-002.mgmt.space.net.local") do |host|
-    host.to_specs.first[:storage].should eql({
-      '/'.to_sym =>  {
-        :type       => 'os',
-        :size       => '99G',
-        :persistent => false,
-        :prepare => {
-          :method => 'image',
-          :options => {
-            :path => '/var/local/images/gold/generic.img'
-          },
-        },
-      }
-    })
-  end
-end
-
-describe_stack 'allow override the gold image location' do
-  given do
-    stack 'demo' do
-      standalone_appserver 'override' do
-        each_machine do |machine|
-          machine.modify_storage({
-            '/' => {
-              :prepare => {
-                :options => {
-                  :path => '/tmp/goldielocks.img'
-                },
-              },
-            }
-          })
-        end
-      end
-    end
-    env "e1", { :primary_site=>"space" } do
-      instantiate_stack "demo"
-    end
-  end
-
-  host("e1-override-002.mgmt.space.net.local") do |host|
-    host.to_specs.first[:storage].should eql({
-      '/'.to_sym =>  {
-        :type       => 'os',
-        :size       => '3G',
-        :persistent => false,
-        :prepare => {
-          :method => 'image',
-          :options => {
-            :path => '/tmp/goldielocks.img'
-          },
-        },
-      }
-    })
-  end
-end
-
-describe_stack 'allow override the method' do
-  given do
-    stack 'demo' do
-      standalone_appserver 'override' do
-        each_machine do |machine|
-          machine.modify_storage({
-            '/' => {
-              :prepare => {
-                :method => 'format',
-              },
-            },
-          })
-        end
-      end
-    end
-    env "e1", { :primary_site=>"space" } do
-      instantiate_stack "demo"
-    end
-  end
-
-  host("e1-override-002.mgmt.space.net.local") do |host|
-    host.to_specs.first[:storage].should eql({
-      '/'.to_sym =>  {
-        :type       => 'os',
-        :size       => '3G',
-        :persistent => false,
-        :prepare    => {
-          :method => 'format',
-          :options => {
-            :path => '/var/local/images/gold/generic.img'
-          },
-        },
-      }
-    })
+    host.to_specs.first[:storage]['/'.to_sym][:size].should eql('99G')
   end
 end
 
 
-describe_stack 'allow additional storage to be provided' do
+describe_stack 'allow additional storage to be specified' do
   given do
     stack 'demo' do
       standalone_appserver 'mysqldb' do
@@ -152,34 +52,24 @@ describe_stack 'allow additional storage to be provided' do
   end
 
   host("e1-mysqldb-002.mgmt.space.net.local") do |host|
-    host.to_specs.first[:storage].should eql({
-      '/'.to_sym =>  {
-        :type       => 'os',
-        :size       => '3G',
-        :persistent => false,
-        :prepare    => {
-          :method => 'image',
-          :options => {
-            :path => '/var/local/images/gold/generic.img'
-          },
-        },
-      },
-      '/var/lib/mysql'.to_sym =>  { :type => 'data', :size => '50G' },
-    })
+    host.to_specs.first[:storage]['/var/lib/mysql'.to_sym][:type].should eql('data')
+    host.to_specs.first[:storage]['/var/lib/mysql'.to_sym][:size].should eql('50G')
   end
 end
 
-describe_stack 'allow existing storage to be modified' do
+describe_stack 'allow all existing storage options to be modified' do
   given do
     stack 'demo' do
       standalone_appserver 'mysqldb' do
         each_machine do |machine|
           machine.modify_storage({
             '/'              => {
+              :type => 'wizzy',
+              :size => '5G',
               :prepare => {
                 :method => 'image',
                 :options => {
-                  :path => '/var/local/images/gold/generic.img'
+                  :path => '/var/local/images/gold/duck.img'
                 },
               },
             },
@@ -203,29 +93,14 @@ describe_stack 'allow existing storage to be modified' do
   end
 
   host("e1-mysqldb-002.mgmt.space.net.local") do |host|
-    host.to_specs.first[:storage].should eql({
-      '/'.to_sym              =>  {
-        :type       => 'os',
-        :size       => '3G',
-        :persistent => false,
-        :prepare => {
-          :method => 'image',
-          :options => {
-            :path => '/var/local/images/gold/generic.img'
-          },
-        },
-      },
-      '/var/lib/mysql'.to_sym =>  {
-        :type       => 'data',
-        :size       => '500G',
-        :prepare => {
-          :method => 'format',
-          :options => {
-            :type => 'ext4'
-          },
-        },
-      },
-    })
+    host.to_specs.first[:storage]['/'.to_sym][:type].should eql('wizzy')
+    host.to_specs.first[:storage]['/'.to_sym][:size].should eql('5G')
+    host.to_specs.first[:storage]['/'.to_sym][:prepare][:method].should eql('image')
+    host.to_specs.first[:storage]['/'.to_sym][:prepare][:options][:path].should eql '/var/local/images/gold/duck.img'
+    host.to_specs.first[:storage]['/var/lib/mysql'.to_sym][:type].should eql('data')
+    host.to_specs.first[:storage]['/var/lib/mysql'.to_sym][:size].should eql('500G')
+    host.to_specs.first[:storage]['/var/lib/mysql'.to_sym][:prepare][:method].should eql('format')
+    host.to_specs.first[:storage]['/var/lib/mysql'.to_sym][:prepare][:options][:type].should eql 'ext4'
   end
 end
 
