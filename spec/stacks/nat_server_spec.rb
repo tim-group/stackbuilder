d9d5196de574740fe213fb1b8ee435d3a7c470c4
Richard Pearce <richard.pearce@timgroup.com>
2014-08-19 15:33:04 +0100

    rpearce: Fix nat server spec, must be bound to env

--- spec/stacks/nat_server_spec.rb
+++ spec/stacks/nat_server_spec.rb
@@ -2,21 +2,17 @@ require 'stacks/environment'
 
 describe Stacks::NatServer do
 
-  subject do
-
+  it 'has access to the front, prod and mgmt networks' do
     class Group
       attr_accessor :name
-
       def initialize(name)
         @name = name
       end
     end
-
-    Stacks::NatServer.new(Group.new("my-nat-server"), "001")
-  end
-
-  it 'has access to the front, prod and mgmt networks' do
-
-    subject.to_specs[0][:networks].should eql [:mgmt,:prod,:front]
+    machinedef = Stacks::NatServer.new(Group.new("my-nat-server"), "001")
+    #machinedef = Stacks::MachineDef.new("test")
+    env = Stacks::Environment.new("noenv", {:primary_site=>"local"}, {})
+    machinedef.bind_to(env)
+    machinedef.to_spec[:networks].should eql [:mgmt,:prod,:front]
   end
 end
