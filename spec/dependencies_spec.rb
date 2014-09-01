a1904032ab5b7850e59082b4ebc19e3fbb89bdd5
Richard Pearce <richard.pearce@timgroup.com>
2014-09-01 11:21:49 +0100

    rpearce: Improve tests around dependencies, make it much clearer how they work

--- spec/dependencies_spec.rb
+++ spec/dependencies_spec.rb
@@ -14,43 +14,55 @@ describe_stack 'stack-with-dependencies' do
       virtual_appserver 'exampleapp2' do
         self.groups = ['blue']
         self.application = 'example2'
-        self.depends_on = ["exampleapp"]
+        self.depends_on = ["exampleapp", "exampledb"]
       end
+    end
+    stack "example_db" do
+      mysqldb "exampledb"
     end
 
     env "e1", :primary_site=>"space" do
       instantiate_stack "example"
+      instantiate_stack "example_db"
       instantiate_stack "loadbalancer"
     end
   end
 
-  host("e1-exampleapp2-002.mgmt.space.net.local") do |host|
-    host.to_enc.should eql(
-       {"role::http_app"=>
-         {"group"=>"blue",
-          "vip_fqdn"=>"e1-exampleapp2-vip.space.net.local",
-          "application"=>"example2",
-          "environment" => "e1",
-          "dependencies" => [
-              ['example.url', 'http://e1-exampleapp-vip.space.net.local:8000']
-           ],
-          "dependant_instances" => ['e1-lb-001.space.net.local','e1-lb-002.space.net.local'],
-          'port'        => '8000'
-         }
-    })
+  host("e1-lb-001.mgmt.space.net.local") do |host|
+     host.to_enc["role::loadbalancer"]["virtual_servers"]["e1-exampleapp-vip.space.net.local"]["realservers"]["blue"].should eql([
+      "e1-exampleapp-001.space.net.local",
+      "e1-exampleapp-002.space.net.local"
+     ])
+     host.to_enc["role::loadbalancer"]["virtual_servers"]["e1-exampleapp2-vip.space.net.local"]["realservers"]["blue"].should eql([
+      "e1-exampleapp2-001.space.net.local",
+      "e1-exampleapp2-002.space.net.local"
+     ])
   end
 
-  host("e1-exampleapp-002.mgmt.space.net.local") do |host|
-    host.to_enc.should eql(
-                           {"role::http_app"=>
-                                {"group"=>"blue",
-                                 "vip_fqdn"=>"e1-exampleapp-vip.space.net.local",
-                                 "application"=>"ExAmPLE",
-                                 "environment" => "e1",
-                                 "dependencies" => [],
-                                 "dependant_instances" => ["e1-exampleapp2-001.space.net.local","e1-exampleapp2-002.space.net.local", 'e1-lb-001.space.net.local', 'e1-lb-002.space.net.local'],
-                                 'port'        => '8000'
-                                }
-                           })
+  host("e1-exampleapp2-002.mgmt.space.net.local") do |host|
+    host.to_enc["role::http_app"]["dependant_instances"].should eql([
+        'e1-lb-001.space.net.local',
+        'e1-lb-002.space.net.local'
+    ])
+    host.to_enc["role::http_app"]["dependencies"].should eql([
+       ['example.url', 'http://e1-exampleapp-vip.space.net.local:8000']
+    ])
   end
+  host("e1-exampleapp-002.mgmt.space.net.local") do |host|
+    host.to_enc["role::http_app"]["dependant_instances"].should eql([
+      "e1-exampleapp2-001.space.net.local",
+      "e1-exampleapp2-002.space.net.local",
+      'e1-lb-001.space.net.local',
+      'e1-lb-002.space.net.local'
+    ])
+    host.to_enc["role::http_app"]["dependencies"].should eql([])
+  end
+  host("e1-exampledb-001.mgmt.space.net.local") do |host|
+    host.to_enc["role::databaseserver"]["dependant_instances"].should eql([
+      "e1-exampleapp2-001.space.net.local",
+      "e1-exampleapp2-002.space.net.local"
+    ])
+    host.to_enc["role::databaseserver"]["dependencies"].should eql([])
+  end
+
 end
