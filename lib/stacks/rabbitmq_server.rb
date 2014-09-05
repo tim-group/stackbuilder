053b7950cf2b84ef05e720311c9b871e447b1aaf
Richard Pearce <richard.pearce@timgroup.com>
2014-09-05 09:59:55 +0100

    rpearce: Model dependencies in rabbitmq server

--- lib/stacks/rabbitmq_server.rb
+++ lib/stacks/rabbitmq_server.rb
@@ -12,13 +12,20 @@ class Stacks::RabbitMQServer < Stacks::MachineDef
   end
 
   def to_enc()
-    {
+    enc = {
       'role::rabbitmq_server' => {
         'cluster_nodes' =>  @virtual_service.realserver_prod_fqdns.map { |fqdn| fqdn.split('.')[0] },
         'vip_fqdn' => vip_fqdn,
        },
        'server::default_new_mgmt_net_local' => nil
     }
+    if @virtual_service.dependant_instances and ! @virtual_service.dependant_instances.nil? and @virtual_service.dependant_instances != []
+      enc['role::rabbitmq_server'].merge!({
+        'dependant_instances' => @virtual_service.dependant_instances,
+        'dependencies' => @virtual_service.dependency_config,
+      })
+    end
+    enc
   end
 end
 
--- spec/rabbitmq_spec.rb
+++ spec/rabbitmq_spec.rb
@@ -3,17 +3,26 @@ require 'stacks/test_framework'
 describe_stack 'mongodb' do
   given do
     stack "rabbit" do
-      virtual_rabbitmqserver
+      virtual_rabbitmqserver 'rabbitmq'
+    end
+
+    stack "example" do
+      virtual_appserver 'exampleapp' do
+        self.depends_on = ["rabbitmq"]
+      end
     end
 
     env "e1", :primary_site=>"space" do
       instantiate_stack "rabbit"
+      instantiate_stack "example"
     end
   end
 
   host("e1-rabbitmq-001.mgmt.space.net.local") do |host|
     host.to_enc['role::rabbitmq_server']['vip_fqdn'].should eql("e1-rabbitmq-vip.space.net.local")
     host.to_enc['role::rabbitmq_server']['cluster_nodes'].should eql(['e1-rabbitmq-001', 'e1-rabbitmq-002'])
+    host.to_enc['role::rabbitmq_server']['dependant_instances'].should eql(['e1-exampleapp-001.space.net.local', 'e1-exampleapp-002.space.net.local'])
+    host.to_enc['role::rabbitmq_server']['dependencies'].should eql([])
     host.to_enc.has_key?('server::default_new_mgmt_net_local').should eql true
   end
 
