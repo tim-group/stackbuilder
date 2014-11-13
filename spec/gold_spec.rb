c412491bdba9958c37dbdb5edd1d4f1eff9f1318
Richard Pearce <richard.pearce@timgroup.com>
2014-11-13 17:09:15 +0000

    rpearce: Adding trusty gold image rspec test

--- spec/gold_spec.rb
+++ spec/gold_spec.rb
@@ -4,6 +4,7 @@ describe_stack 'gold image' do
     stack "gold-image" do
       gold 'goldimage' do
         ubuntu 'precise'
+        ubuntu 'trusty'
         win 'xp', 'ie7',
           :master_location => 'http://imageserver.net.local/master/precise/'
         win 'win7', 'ie9',
@@ -25,6 +26,14 @@ describe_stack 'gold image' do
   host("e1-ubuntu-precise-gold.mgmt.space.net.local") do |host|
     host.to_spec[:template].should eql("ubuntu-precise")
     host.to_spec[:hostname].should eql("e1-ubuntu-precise-gold")
+    host.to_spec[:dont_start].should eql(true)
+    host.to_spec[:storage][:/][:prepare][:options][:shrink_after_unmount].should eql(true)
+    host.to_spec[:storage][:/][:prepare][:options][:resize].should eql(false)
+    host.to_spec[:storage][:/][:prepare][:options][:create_in_fstab].should eql(false)
+  end
+  host("e1-ubuntu-trusty-gold.mgmt.space.net.local") do |host|
+    host.to_spec[:template].should eql("ubuntu-trusty")
+    host.to_spec[:hostname].should eql("e1-ubuntu-trusty-gold")
     host.to_spec[:dont_start].should eql(true)
     host.to_spec[:storage][:/][:prepare][:options][:shrink_after_unmount].should eql(true)
     host.to_spec[:storage][:/][:prepare][:options][:resize].should eql(false)
