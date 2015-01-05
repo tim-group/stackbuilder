b5be87149413fb3f337921f2704f7b417ec99f4d
Besam Khidhir <besam.khidhir@timgroup.com>
2015-01-05 11:39:29 +0000

    bkhidhir/nrauch: modify stackbuilder to print failure message if node is not allocated

--- lib/compute/controller.rb
+++ lib/compute/controller.rb
@@ -87,7 +87,9 @@ class Compute::Controller
 
         specs.map do |spec|
           result = @compute_node_client.launch(host, [spec])
-
+          if result.nil?
+            @logger.info("#{host} launch #{spec[:hostname]} result: #{sender}: failed to launch")
+          end
           result.each do |sender, result_hash|
             if not result_hash[spec[:hostname]].nil?
               result_text = result_hash[spec[:hostname]].first
