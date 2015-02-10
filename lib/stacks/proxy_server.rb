3f84446082351a015cb36311dfb55ca0486ea62a
Richard Pearce <richard.pearce@timgroup.com>
2015-02-10 10:28:00 +0000

    rpearce: Add temporary hack to disable enc data output for proxy servers. This is going to be coming from puppet for the shared proxy.

--- lib/stacks/proxy_server.rb
+++ lib/stacks/proxy_server.rb
@@ -1,24 +1,34 @@
 require 'stacks/namespace'
 
 class Stacks::ProxyServer < Stacks::MachineDef
-  attr_reader :virtual_service
+  attr_reader :virtual_service, :disable_enc
 
   def initialize(virtual_service, index)
     super(virtual_service.name + "-" + index)
     @virtual_service = virtual_service
+    @disable_enc = false
   end
 
   def bind_to(environment)
     super(environment)
   end
 
+  def disable_enc
+    @disable_enc = true
+  end
+
   def to_enc
-    service_resources = Hash[virtual_service.downstream_services()]
-    {'role::proxyserver' => {
-        'prod_vip_fqdn' => @virtual_service.vip_fqdn(:prod),
-        'vhosts'  => service_resources,
-        'environment' => environment.name
+    if @disable_enc
+     {}
+    else
+      service_resources = Hash[virtual_service.downstream_services()]
+      {
+        'role::proxyserver' => {
+          'prod_vip_fqdn' => @virtual_service.vip_fqdn(:prod),
+          'vhosts'  => service_resources,
+          'environment' => environment.name
+        }
       }
-    }
+    end
   end
 end
