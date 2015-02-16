39476c9c912d5cec348dd545412451dacbb90f50
Besam Khidhir <besam.khidhir@timgroup.com>
2015-02-16 15:56:59 +0000

    bkhidhir/mmazurek: fix proxy spec test to match new function set_default_ssl_cert()

--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -103,7 +103,7 @@ describe_stack 'proxy servers can have the default ssl cert and vhost ssl certs 
   given do
     stack "exampleproxy" do
       virtual_proxyserver 'exampleproxy' do
-        default_ssl_cert 'test_cert_change'
+        set_default_ssl_cert 'test_cert_change'
         vhost('exampleapp') do
           with_cert 'test_vhost_cert_change'
         end
