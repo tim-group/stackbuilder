0d8d981eb9457da4faae2abc1e4e9228d8757be3
Richard Pearce <richard.pearce@timgroup.com>
2015-04-09 11:40:45 +0100

    rpearce: Removing this as it seems completely pointless.   * Commit history reveals it was born in fbb79865 but does not explain   * the reasoning or intent for its existancee   * Will speak to David and re-instate if required

--- spec/ci_slave_spec.rb
+++ spec/ci_slave_spec.rb
@@ -17,9 +17,6 @@ describe_stack 'jenkins' do
   end
 
   host("e1-jenkinsslave-002.mgmt.space.net.local") do |host|
-    host.should have_ancestory(
-      ["e1", "jenkins", "jenkinsslave", "jenkinsslave-002"])
-
     host.to_enc.should eql('role::cinode_precise' => {})
   end
 end
