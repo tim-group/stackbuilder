cfeaa6ca07d89a6f427857a74718046e807b3a87
Richard Pearce <richard.pearce@timgroup.com>
2015-04-09 10:55:03 +0100

    rpearce: Convert virtual_service_spec tests to use newer test_framework

--- spec/stacks/virtual_service_spec.rb
+++ spec/stacks/virtual_service_spec.rb
@@ -1,52 +1,64 @@
-require 'stacks/environment'
+require 'stacks/test_framework'
 
 describe 'Stacks::VirtualService' do
-  before do
-    extend Stacks::DSL
-  end
+  describe_stack 'provides the default vips when to_vip_spec is run' do
+    given do
+      stack "test" do
+        virtual_appserver 'myvs'
+      end
 
-  it 'provides the default vips when to_vip_spec is run' do
-    stack "test" do
-      virtual_appserver 'myvs'
-    end
-
-    env 'env', :primary_site => 'mars' do
-      instantiate_stack 'test'
-    end
-
-    vip_spec = find('env-myvs-001.mgmt.mars.net.local').virtual_service.to_vip_spec
-    vip_spec[:qualified_hostnames].should eql(:prod => 'env-myvs-vip.mars.net.local')
-  end
-
-  it 'provides the both front and prod vips when if enable_nat is turned on' do
-    stack "test" do
-      virtual_appserver 'myvs' do
-        enable_nat
+      env 'env', :primary_site => 'mars' do
+        instantiate_stack 'test'
       end
     end
 
-    env 'env', :primary_site => 'mars' do
-      instantiate_stack 'test'
+    host('env-myvs-001.mgmt.mars.net.local') do |host|
+      vip_spec = host.virtual_service.to_vip_spec
+      vip_spec[:qualified_hostnames].should eql(:prod => 'env-myvs-vip.mars.net.local')
     end
-
-    vip_spec = find('env-myvs-001.mgmt.mars.net.local').virtual_service.to_vip_spec
-    vip_spec[:qualified_hostnames].should eql(:prod  => 'env-myvs-vip.mars.net.local',
-                                              :front => 'env-myvs-vip.front.mars.net.local')
   end
 
-  it 'allows a virtual service to add a vip on additional networks' do
-    stack "test" do
-      virtual_appserver 'myvs' do
-        add_vip_network :mgmt
+  describe_stack 'provides the both front and prod vips when if enable_nat is turned on' do
+    given do
+      stack "test" do
+        virtual_appserver 'myvs' do
+          enable_nat
+        end
+      end
+
+      env 'env', :primary_site => 'mars' do
+        instantiate_stack 'test'
       end
     end
 
-    env 'env', :primary_site => 'mars' do
-      instantiate_stack 'test'
+    host('env-myvs-001.mgmt.mars.net.local') do |host|
+      vip_spec = host.virtual_service.to_vip_spec
+      vip_spec[:qualified_hostnames].should eql(
+        :prod  => 'env-myvs-vip.mars.net.local',
+        :front => 'env-myvs-vip.front.mars.net.local'
+      )
+    end
+  end
+
+  describe_stack 'allows a virtual service to add a vip on additional networks' do
+    given do
+      stack "test" do
+        virtual_appserver 'myvs' do
+          add_vip_network :mgmt
+        end
+      end
+
+      env 'env', :primary_site => 'mars' do
+        instantiate_stack 'test'
+      end
     end
 
-    vip_spec = find('env-myvs-001.mgmt.mars.net.local').virtual_service.to_vip_spec
-    vip_spec[:qualified_hostnames].should eql(:prod => 'env-myvs-vip.mars.net.local',
-                                              :mgmt => 'env-myvs-vip.mgmt.mars.net.local')
+    host('env-myvs-001.mgmt.mars.net.local') do |host|
+      vip_spec = host.virtual_service.to_vip_spec
+      vip_spec[:qualified_hostnames].should eql(
+        :prod => 'env-myvs-vip.mars.net.local',
+        :mgmt => 'env-myvs-vip.mgmt.mars.net.local'
+      )
+    end
   end
 end
