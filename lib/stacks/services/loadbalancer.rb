ddcf0218d62d38d4d2e52ba3c0f3ef7e482c2b95
Richard Pearce <richard.pearce@timgroup.com>
2015-04-21 12:41:00 +0100

    rpearce: Fix secondary_site for load balancers   * Add new test and tweak existing test

--- lib/stacks/services/loadbalancer.rb
+++ lib/stacks/services/loadbalancer.rb
@@ -3,9 +3,9 @@ require 'stacks/namespace'
 class Stacks::Services::LoadBalancer < Stacks::MachineDef
   attr_accessor :virtual_router_id
 
-  def initialize(virtual_service, index)
+  def initialize(virtual_service, index, networks = [:mgmt, :prod], location = :primary_site)
+    super(virtual_service.name + "-" + index, networks, location)
     @virtual_service = virtual_service
-    super(virtual_service.name + "-" + index)
   end
 
   def bind_to(environment)
@@ -17,7 +17,9 @@ class Stacks::Services::LoadBalancer < Stacks::MachineDef
     enc = super()
     virtual_services_hash = {}
     @virtual_service.virtual_services(Stacks::Services::AbstractVirtualService).map do |virtual_service|
-      virtual_services_hash.merge! virtual_service.to_loadbalancer_config
+      if virtual_service.fabric == fabric
+       virtual_services_hash.merge! virtual_service.to_loadbalancer_config
+      end
     end
     enc['role::loadbalancer'] = {
       'virtual_router_id' => virtual_router_id,
--- spec/loadbalancer_spec.rb
+++ spec/loadbalancer_spec.rb
@@ -1,45 +1,92 @@
 require 'stacks/test_framework'
-describe_stack 'lb' do
+describe_stack 'load balancers in multiple sites create the correct load balancing config in each site' do
   given do
     stack "lb" do
       loadbalancer  do
-        each_machine do |machine|
-          machine.add_route('mgmt_pg_from_mgmt_oy')
-        end
+        @enable_secondary_site = true
       end
     end
 
-    stack 'fr' do
-      virtual_appserver 'frapp' do
-        enable_ehcache
-        self.application = 'futuresroll'
-        self.instances = 2
+    stack "funds" do
+      virtual_appserver 'fundsapp' do
+        @enable_secondary_site = true
       end
     end
 
-    env "e1", :primary_site => "space", :lb_virtual_router_id => 66 do
-      instantiate_stack "lb"
-      instantiate_stack "fr"
+    env 'e1', :primary_site => 'mars', :secondary_site => 'jupiter' do
+      instantiate_stack 'lb'
+      instantiate_stack 'funds'
     end
   end
 
-  host("e1-lb-002.mgmt.space.net.local") do |host|
-    host.to_enc['role::loadbalancer']['virtual_router_id'].should eql(66)
-    host.to_enc['role::loadbalancer']['virtual_servers']['e1-frapp-vip.space.net.local'].should eql(
-      "env" => "e1",
-      "monitor_warn" => 1,
-      "app" => "futuresroll",
-      "realservers" => {
-        "blue" => ["e1-frapp-001.space.net.local", "e1-frapp-002.space.net.local"]
-      },
-      "healthcheck_timeout" => 10
-    )
-    host.to_enc['routes'].should eql("to" => ["mgmt_pg_from_mgmt_oy"])
-    host.to_specs.shift[:qualified_hostnames].should eql(:mgmt => "e1-lb-002.mgmt.space.net.local",
-                                                         :prod => "e1-lb-002.space.net.local")
-    host.to_specs.shift[:availability_group].should eql('e1-lb')
-    host.to_specs.shift[:networks].should eql([:mgmt, :prod])
-    host.to_specs.shift[:hostname].should eql('e1-lb-002')
-    host.to_specs.shift[:domain].should eql('space.net.local')
+  it_stack 'should contain all the expected hosts' do |stack|
+    stack.should have_hosts([
+                            'e1-lb-001.mgmt.mars.net.local',
+                            'e1-lb-002.mgmt.mars.net.local',
+                            'e1-lb-001.mgmt.jupiter.net.local',
+                            'e1-lb-002.mgmt.jupiter.net.local',
+                            'e1-fundsapp-001.mgmt.mars.net.local',
+                            'e1-fundsapp-002.mgmt.mars.net.local',
+                            'e1-fundsapp-001.mgmt.jupiter.net.local',
+                            'e1-fundsapp-002.mgmt.jupiter.net.local',
+    ])
+  end
+  it_stack 'should create load balancers in both sites with the correct lb config' do |stack|
+
+    host("e1-lb-001.mgmt.mars.net.local") do |host|
+      enc = host.to_enc['role::loadbalancer']['virtual_servers']
+      enc['e1-fundsapp-vip.mars.net.local']['realservers']['blue'].should eql(['e1-fundsapp-001.mars.net.local', 'e1-fundsapp-002.mars.net.local'])
+    end
+
+    host("e1-lb-001.mgmt.jupiter.net.local") do |host|
+      enc = host.to_enc['role::loadbalancer']['virtual_servers']
+      enc['e1-fundsapp-vip.jupiter.net.local']['realservers']['blue'].should eql(['e1-fundsapp-001.jupiter.net.local', 'e1-fundsapp-002.jupiter.net.local'])
+    end
   end
 end
+
+describe_stack 'load balancer stack create a secondary server with the correct enc and spec information' do
+   given do
+     stack "lb" do
+       loadbalancer  do
+         each_machine do |machine|
+           machine.add_route('mgmt_pg_from_mgmt_oy')
+         end
+       end
+     end
+
+     stack 'fr' do
+       virtual_appserver 'frapp' do
+         enable_ehcache
+         self.application = 'futuresroll'
+         self.instances = 2
+       end
+     end
+
+     env "e1", :primary_site => "space", :lb_virtual_router_id => 66 do
+       instantiate_stack "lb"
+       instantiate_stack "fr"
+     end
+   end
+
+   host("e1-lb-002.mgmt.space.net.local") do |host|
+     host.to_enc['role::loadbalancer']['virtual_router_id'].should eql(66)
+     host.to_enc['role::loadbalancer']['virtual_servers']['e1-frapp-vip.space.net.local'].should eql(
+       "env" => "e1",
+       "monitor_warn" => 1,
+       "app" => "futuresroll",
+       "realservers" => {
+         "blue" => ["e1-frapp-001.space.net.local", "e1-frapp-002.space.net.local"]
+       },
+       "healthcheck_timeout" => 10
+     )
+     host.to_enc['routes'].should eql("to" => ["mgmt_pg_from_mgmt_oy"])
+     host.to_specs.shift[:qualified_hostnames].should eql(:mgmt => "e1-lb-002.mgmt.space.net.local",
+                                                         :prod => "e1-lb-002.space.net.local")
+     host.to_specs.shift[:availability_group].should eql('e1-lb')
+     host.to_specs.shift[:networks].should eql([:mgmt, :prod])
+     host.to_specs.shift[:hostname].should eql('e1-lb-002')
+     host.to_specs.shift[:domain].should eql('space.net.local')
+   end
+end
+
