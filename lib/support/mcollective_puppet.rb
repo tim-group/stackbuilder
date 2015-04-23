ece63f442ec619127a214895a89b25626461be17
Michal Mazurek <michal.mazurek@timgroup.com>
2015-04-23 12:52:43 +0100

    update puppetmaster for oy in mcollective_puppet

--- lib/support/mcollective_puppet.rb
+++ lib/support/mcollective_puppet.rb
@@ -148,7 +148,7 @@ module Support::MCollectivePuppet
     puppetmaster = case machine_fqdn
                    when /\.mgmt\.st\.net\.local$/ then ["st-puppetmaster-001.mgmt.st.net.local",
                                                         "staging-puppetmaster-001.mgmt.st.net.local"]
-                   when /\.mgmt\.oy\.net\.local$/ then ["antarctica.oyldn.youdevise.com"]
+                   when /\.mgmt\.oy\.net\.local$/ then ["oy-puppetmaster-001.mgmt.oy.net.local"]
                    when /\.mgmt\.pg\.net\.local$/ then ["australia.pgldn.youdevise.com"]
                    when /\.mgmt\.ci\.net\.local$/ then ["ldn-puppet-001.youdevise.com"]
     end
