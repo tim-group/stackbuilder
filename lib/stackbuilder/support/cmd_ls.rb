3c04a7a7829f32e2469c9a34aac79b42e06bf547
Michal Mazurek <michal.mazurek@timgroup.com>
2015-08-20 12:46:41 +0100

    switch from ruby's "optparse" to "getoptlong" add 'enc' to bin/stacks add 'ls short' to bin/stacks

--- lib/stackbuilder/support/cmd_ls.rb
+++ lib/stackbuilder/support/cmd_ls.rb
@@ -1,5 +1,6 @@
 module CMDLs
-  def self.ls(environment)
+  def self.ls(argv, environment)
+    @short = argv.include? 'short'
     traverse('', nil, environment)
   end
 
@@ -8,7 +9,7 @@ module CMDLs
   def self.traverse(indent, is_last, machine_def)
     case machine_def.clazz
     when 'container'
-      if machine_def.respond_to?(:domain_suffix)
+      if machine_def.respond_to?(:domain_suffix) # environment
         type = '[0;35mE[0m'
         rake_task_name = machine_def.name
       else
@@ -28,13 +29,17 @@ module CMDLs
                        when false then ['-> ', ' |', ' |'] # middle item
                        when true then  ['-> ', '  ', ' `'] # last item
     end
+
     printf("[0;33m%s[0m%s %s\n", indent + char + ptr, type, rake_task_name)
     indent += space
 
     return unless machine_def.respond_to?(:children)
 
-    last = machine_def.children.count - 1
-    machine_def.children.each_with_index do |child, index|
+    children = machine_def.children
+    children.select! { |m| m.clazz != 'machine' } if @short
+
+    last = children.count - 1
+    children.each_with_index do |child, index|
       traverse(indent, index == last, child)
     end
   end
