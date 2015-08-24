9c823552baf58eeacff8db6778f4ca71ec0b1193
Michal Mazurek <michal.mazurek@timgroup.com>
2015-08-24 16:04:00 +0100

    fix spec for previous

--- spec/eventstore_spec.rb
+++ spec/eventstore_spec.rb
@@ -17,7 +17,7 @@ describe_stack 'eventstore' do
 
   host("e1-eventstore-001.mgmt.space.net.local") do |host|
     host.to_enc['role::eventstore_server']['clusternodes'].sort.should eql(
-      ["e1-eventstore-001.moon.net.local", "e1-eventstore-001.space.net.local", "e1-eventstore-002.moon.net.local",
+      ["e1-eventstore-001.moon.net.local", "e1-eventstore-002.moon.net.local",
        "e1-eventstore-002.space.net.local", "e1-eventstore-003.moon.net.local", "e1-eventstore-003.space.net.local"].
         sort)
   end
