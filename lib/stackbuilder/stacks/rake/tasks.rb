bddf482d9a103c0203f84a65d4124095a5aefdb6
Michal Mazurek <michal.mazurek@timgroup.com>
2015-08-25 09:55:34 +0100

    be more verbose

--- lib/stackbuilder/stacks/rake/tasks.rb
+++ lib/stackbuilder/stacks/rake/tasks.rb
@@ -105,21 +105,21 @@ namespace :sbx do
   # XXX 2015-08-19 remove after a while
   desc 'Print a report of KVM host CPU/Storage/Memory allocation'
   task :audit_host_machines do
-    puts "replaced by the commandline utility, run: 'stacks audit -e env'"
+    puts "replaced by the commandline utility, run: 'stacks audit -p PATH_TO_STACKBUILDER_CONFIG -e env'"
     exit 1
   end
 
   # XXX 2015-08-19 remove after a while
   desc 'run to_enc on all nodes'
   task :dump_enc do
-    puts "replaced by the commandline utility, run: 'stacks dump_enc'"
+    puts "replaced by the commandline utility, run: 'stacks dump_enc -p PATH_TO_STACKBUILDER_CONFIG'"
     exit 1
   end
 
   # XXX 2015-08-19 remove after a while
   desc 'run to_spec on all nodes'
   task :dump_spec do
-    puts "replaced by the commandline utility, run: 'stacks dump_spec'"
+    puts "replaced by the commandline utility, run: 'stacks dump_spec -p PATH_TO_STACKBUILDER_CONFIG'"
     exit 1
   end
 
