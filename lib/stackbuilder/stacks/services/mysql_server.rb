3c1e38fb67a1e8b313c248ce294688fe9daef97a
Mehul Shah <mehul.shah@timgroup.com>
2015-09-29 13:06:59 +0100

    mshah correcting the names

--- lib/stackbuilder/stacks/services/mysql_server.rb
+++ lib/stackbuilder/stacks/services/mysql_server.rb
@@ -131,8 +131,8 @@ class Stacks::Services::MysqlServer < Stacks::MachineDef
     @config = merge_gtid_config if @use_gtids
 
     unless @virtual_service.allowed_hosts.empty?
-      enc['role::allow_hosts'] = {
-        'allow_hosts' => @virtual_service.allowed_hosts
+      enc['role::mysql_allow_hosts'] = {
+        'hosts' => @virtual_service.allowed_hosts
       }
     end
 
