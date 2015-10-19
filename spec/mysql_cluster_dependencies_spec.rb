322b150818aad30e8df4cd00cccbf097e9a4092a
Graham Allan <graham.allan@timgroup.com>
2015-10-19 17:19:02 +0100

    gallan: cosmetic only -- replace double quote with single.

--- spec/mysql_cluster_dependencies_spec.rb
+++ spec/mysql_cluster_dependencies_spec.rb
@@ -7,23 +7,23 @@ describe_stack 'stack-with-dependencies' do
     stack 'loadbalancer' do
       loadbalancer
     end
-    stack "example" do
+    stack 'example' do
       virtual_appserver 'exampleapp2' do
         self.groups = ['blue']
         self.application = 'example2'
-        if "e1" == environment.name
-          depend_on("exampledb", "e1")
-        elsif "pg" == environment.name
-          depend_on("exampledb", "pg")
+        if 'e1' == environment.name
+          depend_on('exampledb', 'e1')
+        elsif 'pg' == environment.name
+          depend_on('exampledb', 'pg')
         else
-          depend_on("exampledb", "e1")
+          depend_on('exampledb', 'e1')
         end
       end
     end
 
-    stack "example_db" do
+    stack 'example_db' do
       mysql_cluster 'exampledb' do
-        self.database_name = "example"
+        self.database_name = 'example'
         self.backup_instances = 1
         self.slave_instances = 2
         self.secondary_site_slave_instances = 1
@@ -31,57 +31,57 @@ describe_stack 'stack-with-dependencies' do
       end
     end
 
-    env "e1", :primary_site => "space", :secondary_site => "earth" do
+    env 'e1', :primary_site => 'space', :secondary_site => 'earth' do
       env 'pg', :production => true do
-        instantiate_stack "example_db"
-        instantiate_stack "example"
+        instantiate_stack 'example_db'
+        instantiate_stack 'example'
       end
 
-      instantiate_stack "example_db"
-      instantiate_stack "example"
+      instantiate_stack 'example_db'
+      instantiate_stack 'example'
     end
 
-    env "e2", :primary_site => "earth", :secondary_site => "space" do
-      instantiate_stack "loadbalancer"
-      instantiate_stack "example"
+    env 'e2', :primary_site => 'earth', :secondary_site => 'space' do
+      instantiate_stack 'loadbalancer'
+      instantiate_stack 'example'
     end
   end
 
-  host("e2-exampleapp2-002.mgmt.earth.net.local") do |host|
-    deps = host.to_enc["role::http_app"]["dependencies"]
-    expect(deps["db.example.database"]).to eql("example")
-    expect(deps["db.example.hostname"]).to eql("e1-exampledb-001.space.net.local")
-    expect(deps["db.example.port"]).to eql("3306")
-    expect(deps["db.example.password_hiera_key"]).to eql("enc/e2/example2/mysql_password")
-    expect(deps["db.example.username"]).to eql("example2")
-    expect(deps["db.example.read_only_cluster"]).to eql(
-      "e1-exampledb-001.earth.net.local" \
+  host('e2-exampleapp2-002.mgmt.earth.net.local') do |host|
+    deps = host.to_enc['role::http_app']['dependencies']
+    expect(deps['db.example.database']).to eql('example')
+    expect(deps['db.example.hostname']).to eql('e1-exampledb-001.space.net.local')
+    expect(deps['db.example.port']).to eql('3306')
+    expect(deps['db.example.password_hiera_key']).to eql('enc/e2/example2/mysql_password')
+    expect(deps['db.example.username']).to eql('example2')
+    expect(deps['db.example.read_only_cluster']).to eql(
+      'e1-exampledb-001.earth.net.local' \
     )
   end
 
-  host("e1-exampleapp2-002.mgmt.space.net.local") do |host|
-    deps = host.to_enc["role::http_app"]["dependencies"]
+  host('e1-exampleapp2-002.mgmt.space.net.local') do |host|
+    deps = host.to_enc['role::http_app']['dependencies']
 
-    expect(deps["db.example.database"]).to eql("example")
-    expect(deps["db.example.hostname"]).to eql("e1-exampledb-001.space.net.local")
-    expect(deps["db.example.port"]).to eql("3306")
-    expect(deps["db.example.password_hiera_key"]).to eql("enc/e1/example2/mysql_password")
-    expect(deps["db.example.username"]).to eql("example2")
-    expect(deps["db.example.read_only_cluster"]).to eql(
-      "e1-exampledb-002.space.net.local,e1-exampledb-003.space.net.local" \
+    expect(deps['db.example.database']).to eql('example')
+    expect(deps['db.example.hostname']).to eql('e1-exampledb-001.space.net.local')
+    expect(deps['db.example.port']).to eql('3306')
+    expect(deps['db.example.password_hiera_key']).to eql('enc/e1/example2/mysql_password')
+    expect(deps['db.example.username']).to eql('example2')
+    expect(deps['db.example.read_only_cluster']).to eql(
+      'e1-exampledb-002.space.net.local,e1-exampledb-003.space.net.local' \
     )
   end
 
-  host("pg-exampleapp2-002.mgmt.space.net.local") do |host|
-    deps = host.to_enc["role::http_app"]["dependencies"]
+  host('pg-exampleapp2-002.mgmt.space.net.local') do |host|
+    deps = host.to_enc['role::http_app']['dependencies']
 
-    expect(deps["db.example.database"]).to eql("example")
-    expect(deps["db.example.hostname"]).to eql("pg-exampledb-001.space.net.local")
-    expect(deps["db.example.port"]).to eql("3306")
-    expect(deps["db.example.password_hiera_key"]).to eql("enc/pg/example2/mysql_password")
-    expect(deps["db.example.username"]).to eql("example2")
-    expect(deps["db.example.read_only_cluster"]).to eql(
-      "pg-exampledb-002.space.net.local,pg-exampledb-003.space.net.local" \
+    expect(deps['db.example.database']).to eql('example')
+    expect(deps['db.example.hostname']).to eql('pg-exampledb-001.space.net.local')
+    expect(deps['db.example.port']).to eql('3306')
+    expect(deps['db.example.password_hiera_key']).to eql('enc/pg/example2/mysql_password')
+    expect(deps['db.example.username']).to eql('example2')
+    expect(deps['db.example.read_only_cluster']).to eql(
+      'pg-exampledb-002.space.net.local,pg-exampledb-003.space.net.local' \
     )
   end
 end
