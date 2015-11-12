7b0d84d5397f2e81bc8147523b4ad3027e337aba
Steve Haslam <steve.haslam@timgroup.com>
2015-11-12 14:45:23 +0000

    SteveH/RichardP: allocate storage, CPUs and memory for elasticsearch nodes

--- lib/stackbuilder/stacks/services/elasticsearch_node.rb
+++ lib/stackbuilder/stacks/services/elasticsearch_node.rb
@@ -5,6 +5,23 @@ class Stacks::Services::ElasticsearchNode < Stacks::MachineDef
   def initialize(virtual_service, index)
     @virtual_service = virtual_service
     super(virtual_service.name + "-" + index)
+
+    @ram = '16777216' # 16GB
+    @vcpus = '4'
+
+    storage = {
+      '/tmp' => {
+        :type       => 'os',
+        :size       => '10G',
+        :persistent => false
+      },
+      '/mnt/data' => {
+        :type       => 'data',
+        :size       => '10G',
+        :persistent => true
+      }
+    }
+    modify_storage(storage)
   end
 
   def to_enc
--- spec/elasticsearch_spec.rb
+++ spec/elasticsearch_spec.rb
@@ -24,3 +24,19 @@ describe_stack 'should provide 3 elasticsearch nodes by default' do
     expect(host.to_enc.key?('role::elasticsearch_node')).to eql(true)
   end
 end
+
+describe_stack 'should provide a default of 16GB of ram and 4 cpu cores' do
+  given do
+    stack "elasticsearch" do
+      elasticsearch_cluster "logs" do
+      end
+    end
+    env "testing", :primary_site => "space", :secondary_site => "earth" do
+      instantiate_stack "elasticsearch"
+    end
+  end
+  host("testing-logs-001.mgmt.space.net.local") do |host|
+    expect(host.to_specs.shift[:ram]).to eql '16777216'
+    expect(host.to_specs.shift[:vcpus]).to eql '4'
+  end
+end
