fdae6515962ba6f81e2a2809414e852e1ee8458f
Steve Haslam <steve.haslam@timgroup.com>
2015-11-13 19:31:21 +0000

    SteveH: include default JVM flags distributed with ElasticSearch

--- lib/stackbuilder/stacks/services/elasticsearch_cluster.rb
+++ lib/stackbuilder/stacks/services/elasticsearch_cluster.rb
@@ -13,6 +13,7 @@ module Stacks::Services::ElasticsearchCluster
     @cluster_name = @name
     @application = 'ElasticsearchApp'
     @instances = 3
-    @jvm_args = '-Xmx8g -Xms8g'
+    @jvm_args = '-Xmx8g -Xms8g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75' \
+                ' -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC'
   end
 end
