f656936376bb87db06919687fca7feac4f5132ed
Richard Pearce <richard.pearce@timgroup.com>
2016-04-05 15:45:01 +0100

    Revert "Revert "rpearce: Ensure we exit on faliure when system stacks system calls return false (failure).""
    This reverts commit e631e021509fc343973f748470366724a161c416.

--- lib/stackbuilder/support/cmd.rb
+++ lib/stackbuilder/support/cmd.rb
@@ -100,8 +100,9 @@ class CMD
   # XXX do this properly
   def clean_all(_argv)
     machine_def = check_and_get_stack
-    system("cd #{$options[:path]} && env=#{$environment.name} rake sbx:#{machine_def.identity}:clean")
-    system("cd #{$options[:path]} && env=#{$environment.name} rake sbx:#{machine_def.identity}:clean_traces")
+    system("cd #{$options[:path]}" \
+           " && env=#{$environment.name} rake sbx:#{machine_def.identity}:clean" \
+           " && env=#{$environment.name} rake sbx:#{machine_def.identity}:clean_traces")
   end
 
   # XXX do this properly
