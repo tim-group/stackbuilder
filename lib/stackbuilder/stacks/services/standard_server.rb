4c00088fe5b2e58721e32ddb227d44ce9b9dca12
Richard Pijnenburg <richard.pijnenburg@timgroup.com>
2016-06-27 15:07:18 +0100

    Expose allow_host stuff for standard server

--- lib/stackbuilder/stacks/services/standard_server.rb
+++ lib/stackbuilder/stacks/services/standard_server.rb
@@ -7,8 +7,16 @@ class Stacks::Services::StandardServer < Stacks::MachineDef
     super(virtual_service.name + "-" + index)
   end
 
+  def allowed_hosts_enc
+    return {} if @virtual_service.allowed_hosts.empty?
+    {
+      'allowed_hosts' => @virtual_service.allowed_hosts
+    }
+  end
+
   def to_enc
     enc = super
+    enc.merge!(allowed_hosts_enc)
     enc.merge!('server::default_new_mgmt_net_local' => {})
     enc
   end
--- spec/standard_server_spec.rb
+++ spec/standard_server_spec.rb
@@ -4,7 +4,9 @@ require 'stacks/test_framework'
 describe_stack 'standard' do
   given do
     stack "standard" do
-      standard "mymachine"
+      standard "mymachine" do
+        allow_host '0.0.0.0/0'
+      end
     end
 
     env "e1", :primary_site => "space" do
@@ -14,6 +16,7 @@ describe_stack 'standard' do
 
   host("e1-mymachine-001.mgmt.space.net.local") do |host|
     expect(host.to_enc['server::default_new_mgmt_net_local']).to eql({})
+    expect(host.to_enc['allowed_hosts']).to eql(['0.0.0.0/0'])
   end
 end
 
