5b4c9e7ae640d74d9e1fb05e23824ed2749d36e2
Richard Pearce <richard.pearce@timgroup.com>
2017-02-09 12:57:10 +0000

    rpearce: Switch to using a boolean for monitoring and a seperate hash for the options

--- lib/stackbuilder/stacks/machine_def.rb
+++ lib/stackbuilder/stacks/machine_def.rb
@@ -23,6 +23,7 @@ class Stacks::MachineDef
   attr_accessor :site
   attr_accessor :monitoring
   attr_accessor :monitoring_in_enc
+  attr_accessor :monitoring_options
 
   def initialize(virtual_service, base_hostname, environment, site, role = nil)
     @virtual_service = virtual_service
@@ -45,12 +46,14 @@ class Stacks::MachineDef
         }
       }
     }
-    @monitoring = {
-      'checks'     => true,
-      'importance' => 'low'
+    @monitoring = true
+    @monitoring_options = {
+      'nagios_host_template'    => 'non-prod-host',
+      'nagios_service_template' => 'non-prod-service'
     }
     @monitoring = @virtual_service.monitoring if @virtual_service.respond_to?(:monitoring)
-    @monitoring_in_enc = false # temporary flag
+    @monitoring_options = @virtual_service.monitoring_options if @virtual_service.respond_to?(:monitoring_options)
+    @monitoring_in_enc = false # temporary feature flag
     @monitoring_in_enc = @virtual_service.monitoring_in_enc if @virtual_service.respond_to?(:monitoring_in_enc)
 
     @destroyable = true
@@ -217,7 +220,12 @@ class Stacks::MachineDef
 
   def to_enc
     enc = {}
-    enc['monitoring'] = @monitoring if @monitoring_in_enc
+    if @monitoring_in_enc
+      enc['monitoring'] = {
+        'checks'  => @monitoring,
+        'options' => @monitoring_options
+      }
+    end
     enc.merge! @included_classes unless @included_classes.nil?
     enc.merge! @virtual_service.included_classes if @virtual_service && @virtual_service.respond_to?(:included_classes)
     unless @routes.empty?
--- lib/stackbuilder/stacks/machine_set.rb
+++ lib/stackbuilder/stacks/machine_set.rb
@@ -15,6 +15,7 @@ class Stacks::MachineSet
   attr_accessor :server_offset
   attr_accessor :role_in_name
   attr_accessor :monitoring
+  attr_accessor :monitoring_options
   attr_accessor :monitoring_in_enc
   attr_reader :allowed_hosts
   attr_reader :default_networks
@@ -40,11 +41,12 @@ class Stacks::MachineSet
     @server_offset = 0
     @add_role_to_name = []
     @role_in_name = false
-    @monitoring = {
-      :checks     => true,
-      :importance => :low
+    @monitoring = true
+    @monitoring_options = {
+      'nagios_host_template'    => 'non-prod-host',
+      'nagios_service_template' => 'non-prod-service'
     }
-    @monitoring_in_enc = false # temporary flag
+    @monitoring_in_enc = false # temporary feature flag
   end
 
   def secondary_site?
