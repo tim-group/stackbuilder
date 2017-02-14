e995f27cd0d827859cd82a8a851108cb9b995800
Gary Richards <gary+github@dsnine.co.uk>
2017-02-14 16:15:17 +0000

    Correct the nagios servers used for downtime scheduling
    antarctica and australia have gone away

--- lib/stackbuilder/support/nagios.rb
+++ lib/stackbuilder/support/nagios.rb
@@ -32,8 +32,8 @@ module Support
       def initialize(options)
         ## FIXME: This does not belong here, but we dont know where it should go
         default_nagios_servers = {
-          'oy' => ['antarctica.mgmt.oy.net.local', 'oy-nagios-001.mgmt.oy.net.local'],
-          'pg' => ['australia.mgmt.pg.net.local']
+          'oy' => ['oy-nagios-001.mgmt.oy.net.local'],
+          'pg' => ['pg-nagios-001.mgmt.pg.net.local']
         }
         default_api_port = 5152
         @nagios_servers = options[:nagios_servers] || default_nagios_servers
