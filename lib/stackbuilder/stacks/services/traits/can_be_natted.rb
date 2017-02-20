3667c8d369862684d1f6906e4fb70baa1463369b
Gary Richards <gary+github@dsnine.co.uk>
2017-02-20 14:11:05 +0000

    Add back in the functionality allowing to NAT through to a specific host
    Revert "Remove the ability to NAT to a specific host. This functionality was not required after all."
    This reverts commit 139ba07e99a797132a1e38d35e12ddbf965fc2cf.

--- lib/stackbuilder/stacks/services/traits/can_be_natted.rb
+++ lib/stackbuilder/stacks/services/traits/can_be_natted.rb
@@ -30,29 +30,46 @@ module Stacks::Services::CanBeNatted
   attr_accessor :nat_config
 
   def configure
-    configure_nat(false, false, :front, :prod, true, false)
+    @nat_config = NatConfig.new(false, false, :front, :prod, true, false)
   end
 
   def configure_nat(dnat_enabled, snat_enabled, public_network, private_network, tcp, udp)
     @nat_config = NatConfig.new(dnat_enabled, snat_enabled, public_network, private_network, tcp, udp)
   end
 
-  def calculate_nat_rules(type, site)
-    vip_hostname = "#{environment.name}-#{name}-vip"
+  def configure_dnat(public_network, private_network, tcp, udp)
+    configure_nat(true, nat_config.snat_enabled, public_network, private_network, tcp, udp)
+  end
+
+  def configure_snat(public_network, private_network, tcp, udp)
+    configure_nat(nat_config.dnat_enabled, true, public_network, private_network, tcp, udp)
+  end
+
+  def calculate_nat_rules(type, site, requirements)
+    hostnames = requirements.map do |requirement|
+      case requirement
+      when :nat_to_host
+        children.map(&:hostname)
+      when :nat_to_vip
+        ["#{environment.name}-#{name}-vip"]
+      end
+    end.flatten
 
     case type
     when :dnat
-      nat_config.dnat_enabled ? create_rules_for_hosts(vip_hostname, site, :dnat) : []
+      nat_config.dnat_enabled ? create_rules_for_hosts(hostnames, site, :dnat) : []
     when :snat
-      nat_config.snat_enabled ? create_rules_for_hosts(vip_hostname, site, :snat) : []
+      nat_config.snat_enabled ? create_rules_for_hosts(hostnames, site, :snat) : []
     end
   end
 
   private
 
-  def create_rules_for_hosts(hostname, site, type)
+  def create_rules_for_hosts(hostnames, site, type)
     ports.map do |port|
-      nat_config.create_rule(environment, type, hostname, site, port)
+      hostnames.map do |hostname|
+        nat_config.create_rule(environment, type, hostname, site, port)
+      end
     end.flatten
   end
 end
