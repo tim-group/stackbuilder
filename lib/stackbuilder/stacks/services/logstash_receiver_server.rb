740fbd1e69aa85eaa66a32ace7c40bb7544244af
Graham Allan <graham.allan@timgroup.com>
2017-03-01 19:34:13 +0000

    gallan: store the virtual server in a field that's more understandable.

--- lib/stackbuilder/stacks/services/logstash_receiver_server.rb
+++ lib/stackbuilder/stacks/services/logstash_receiver_server.rb
@@ -2,25 +2,26 @@ require 'stackbuilder/stacks/namespace'
 require 'stackbuilder/stacks/machine_def'
 
 class Stacks::Services::LogstashReceiverServer < Stacks::MachineDef
-  attr_accessor :role
+  attr_reader :logstash_cluster
 
   def initialize(virtual_service, base_hostname, environment, site, role)
     super(virtual_service, base_hostname, environment, site, role)
+    @logstash_cluster = virtual_service
   end
 
   def stackname
-    @virtual_service.name
+    @logstash_cluster.name
   end
 
   def to_enc
     enc = super()
 
-    rabbitmq_config = @virtual_service.rabbitmq_config
+    rabbitmq_config = @logstash_cluster.rabbitmq_config
 
     enc.merge!('role::logstash_receiver' => {
                  'rabbitmq_logging_username' => rabbitmq_config.username,
                  'rabbitmq_logging_password_key' => rabbitmq_config.password_hiera_key,
-                 'rabbitmq_logging_exchange' => @virtual_service.exchange
+                 'rabbitmq_logging_exchange' => @logstash_cluster.exchange
                })
     enc
   end
--- lib/stackbuilder/stacks/services/rabbitmq_logging_server.rb
+++ lib/stackbuilder/stacks/services/rabbitmq_logging_server.rb
@@ -2,12 +2,15 @@ require 'stackbuilder/stacks/namespace'
 require 'stackbuilder/stacks/machine_def'
 
 class Stacks::Services::RabbitMqLoggingServer < Stacks::MachineDef
+  attr_reader :rabbitmq_logging_cluster
+
   def initialize(virtual_service, base_hostname, environment, site, role)
     super(virtual_service, base_hostname, environment, site, role)
+    @rabbitmq_logging_cluster = virtual_service
   end
 
   def stackname
-    @virtual_service.name
+    @rabbitmq_logging_cluster.name
   end
 
   def to_enc
