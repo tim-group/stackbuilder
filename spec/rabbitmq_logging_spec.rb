8d01d76b14f6dd027dbc2b25042e1396445a7eda
Graham Allan <graham.allan@timgroup.com>
2017-03-01 19:26:52 +0000

    gallan: invert the test cases so the more complicated one has less noise.

--- spec/rabbitmq_logging_spec.rb
+++ spec/rabbitmq_logging_spec.rb
@@ -5,7 +5,6 @@ describe_stack 'rabbitmq logging cluster' do
   given do
     stack 'per_site_log_collecting' do
       logstash_receiver 'logstash-receiver' do
-        self.exchange = 'my-logging-exchange'
       end
 
       # TODO: add to custom_services, create classes, etc.
@@ -23,25 +22,27 @@ describe_stack 'rabbitmq logging cluster' do
     role_enc = host.to_enc['role::logstash_receiver']
     expect(role_enc['rabbitmq_logging_username']).to eql('logstash_receiver')
     expect(role_enc['rabbitmq_logging_password_key']).to eql('e1/logstash_receiver/messaging_password')
-    expect(role_enc['rabbitmq_logging_exchange']).to eql('my-logging-exchange')
+    expect(role_enc['rabbitmq_logging_exchange']).to eql('logging')
 
     # TODO: depend_on and get logging hosts
     # expect(role_enc['rabbitmq_logging_hosts']).to eql(['e1-rabbitmq-logging-001.mgmt.space.net.local', 'e1-rabbitmq-logging-001.mgmt.space.net.local'])
   end
 end
 
-describe_stack 'rabbitmq logging defaults' do
+describe_stack 'rabbitmq logging exchange is configurable' do
   given do
-    stack 'defaults' do
-      logstash_receiver 'logstash-receiver'
+    stack 'logstash_receiver' do
+      logstash_receiver 'logstash-receiver' do
+        self.exchange = 'my-logging-exchange'
+      end
   end
 
     env 'e1', :primary_site => 'space' do
-      instantiate_stack 'defaults'
+      instantiate_stack 'logstash_receiver'
     end
   end
 
   host('e1-logstash-receiver-001.mgmt.space.net.local') do |host|
-    expect(host.to_enc['role::logstash_receiver']['rabbitmq_logging_exchange']).to eql('logging')
+    expect(host.to_enc['role::logstash_receiver']['rabbitmq_logging_exchange']).to eql('my-logging-exchange')
   end
 end
