32e4b1f0a8fce0ea8abf365a62e11c9cae8bcadf
Graham Allan <graham.allan@timgroup.com>
2017-03-01 22:35:47 +0000

    gallan: minor formatting.

--- lib/stackbuilder/stacks/services/rabbitmq_server.rb
+++ lib/stackbuilder/stacks/services/rabbitmq_server.rb
@@ -7,10 +7,6 @@ class Stacks::Services::RabbitMQServer < Stacks::MachineDef
   end
 
   def to_enc
-    dependant_instances = @rabbitmq_cluster.dependant_instance_fqdns(location)
-    dependant_instances.concat(@rabbitmq_cluster.children.map(&:prod_fqdn)).sort
-    dependant_instances.delete prod_fqdn
-
     enc = super()
     enc.merge!('role::rabbitmq_server' => {
                  'cluster_nodes' =>  @rabbitmq_cluster.cluster_nodes(location),
@@ -18,12 +14,17 @@ class Stacks::Services::RabbitMQServer < Stacks::MachineDef
                },
                'server::default_new_mgmt_net_local' => nil)
 
+    dependant_instances = @rabbitmq_cluster.dependant_instance_fqdns(location)
+    dependant_instances.concat(@rabbitmq_cluster.children.map(&:prod_fqdn)).sort
+    dependant_instances.delete prod_fqdn
+
     if !dependant_instances.nil? && dependant_instances != []
       enc['role::rabbitmq_server'].merge!('dependant_instances' => dependant_instances,
                                           'dependencies'        => @rabbitmq_cluster.dependency_config(fabric),
                                           'dependant_users'     => @rabbitmq_cluster.dependant_users,
                                           'rabbit_users'        => [])
     end
+
     enc
   end
 end
