d75b45777d790e22bb5d36895ac31b70c65c3528
Graham Allan <graham.allan@timgroup.com>
2017-08-25 14:49:25 +0100

    gallan: add two commands for inspecting dependencies.
     - dependencies for listing which boxes a node depends on
     - dependents for listing which boxes depend on the specified node
     - this does not (yet) take account of requirements, so for example, if an appserver depends on a specific instance  of mysql, that's not reflected in the output
     - allow executing stacks command with local code, using the dev flag on the environment

--- lib/stackbuilder/support/cmd.rb
+++ lib/stackbuilder/support/cmd.rb
@@ -413,6 +413,22 @@ class CMD
     end
   end
 
+  def dependencies(_argv)
+    machine_def = ensure_is_machine(check_and_get_stack)
+
+    service_dependencies = machine_def.virtual_service.virtual_services_that_i_depend_on
+    dependencies = service_dependencies.map { |machine_set| machine_set.children.map(&:identity) }.flatten
+    puts ZAMLS.to_zamls(dependencies)
+  end
+
+  def dependents(_argv)
+    machine_def = ensure_is_machine(check_and_get_stack)
+
+    service_dependencies = machine_def.virtual_service.virtual_services_that_depend_on_me
+    dependencies = service_dependencies.map { |machine_set| machine_set.children.map(&:identity) }.flatten
+    puts ZAMLS.to_zamls(dependencies)
+  end
+
   private
 
   def ensure_is_machine(machine_def)
