1f8ffcbf0fe119203bced37a9a98d8eda6e88357
Mehul Shah <mehul.shah@timgroup.com>
2017-09-14 16:09:16 +0100

    mshah - including all nodes in the cluster

--- lib/stackbuilder/stacks/services/kafka_server.rb
+++ lib/stackbuilder/stacks/services/kafka_server.rb
@@ -4,7 +4,7 @@ require 'stackbuilder/stacks/machine_def'
 class Stacks::Services::KafkaServer < Stacks::MachineDef
   def to_enc
     enc = super()
-    siblings = @virtual_service.children.select { |child| child != self }
+    siblings = @virtual_service.children
     enc.merge!('role::kafka_server' => {
                  'clusternodes'        => siblings.map(&:prod_fqdn).sort,
                  'dependant_instances' => @virtual_service.dependant_instance_fqdns(location, [:prod], false)
