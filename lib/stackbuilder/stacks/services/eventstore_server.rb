fa05c4bfb06b67a8a0436de5e428266ea87b759d
Mehul Shah <mehul.shah@timgroup.com>
2017-09-15 09:33:40 +0100

    mshah - including hostname of node in the cluster

--- lib/stackbuilder/stacks/services/eventstore_server.rb
+++ lib/stackbuilder/stacks/services/eventstore_server.rb
@@ -4,8 +4,9 @@ require 'stackbuilder/stacks/machine_def'
 class Stacks::Services::EventStoreServer < Stacks::MachineDef
   def to_enc
     enc = super()
-    siblings = @virtual_service.children.select { |child| child != self }
+    siblings = @virtual_service.children
     enc.merge!('role::eventstore_server' => {
+                 'hostname'            => :hostname,
                  'clusternodes'        => siblings.map(&:prod_fqdn).sort,
                  'dependant_instances' => @virtual_service.dependant_instance_fqdns(location, [:prod], false)
                })
