a78c9bdd073f7ed72a9b5f80d02a7bf9a240d070
Adam Gibbins <adam.gibbins@acuris.com>
2018-01-29 12:24:45 +0000

    Add LON nagios server

--- lib/stackbuilder/support/nagios.rb
+++ lib/stackbuilder/support/nagios.rb
@@ -37,7 +37,8 @@ module Support
         ## FIXME: This does not belong here, but we dont know where it should go
         default_nagios_servers = {
           'oy' => ['oy-nagios-001.mgmt.oy.net.local'],
-          'pg' => ['pg-nagios-001.mgmt.pg.net.local']
+          'pg' => ['pg-nagios-001.mgmt.pg.net.local'],
+          'lon' => ['lon-nagios-001.mgmt.lon.net.local']
         }
         default_api_port = 5152
         @nagios_servers = options[:nagios_servers] || default_nagios_servers
