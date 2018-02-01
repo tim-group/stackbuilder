1fd7b7a6265db9090b9ba88d1a2a99e613314274
Tom Denley <tom.denley@timgroup.com>
2018-02-01 16:52:28 +0000

    tomd: shoot dead code

--- lib/stackbuilder/support/cmd.rb
+++ lib/stackbuilder/support/cmd.rb
@@ -96,43 +96,6 @@ class CMD
     end
   end
 
-  # XXX work in progress
-  def puppet(argv)
-    logger(Logger::FATAL) { "work in progress" }
-    exit 1
-
-    cmd = argv.shift
-    if cmd.nil? then
-      logger(Logger::FATAL) { 'puppet needs a subcommand' }
-      exit 1
-    end
-
-    machine_def = check_and_get_stack
-
-    logger(Logger::DEBUG) { "about to run puppet \"#{cmd}\" on \"#{machine_def.name}\"" }
-
-    require 'stackbuilder/support/puppet'
-    pctl = PuppetCtl.new
-
-    case cmd
-    when 'sign'
-      pctl.sign(machine_def)
-    when 'poll_sign'
-      pctl.poll_sign(machine_def)
-    when 'wait'
-      pctl.wait(machine_def)
-    when 'run'
-      pctl.run(machine_def)
-    when 'clean'
-      pctl.clean(machine_def)
-    when 'clean_all'
-      pctl.clean_all(machine_def)
-    else
-      logger(Logger::FATAL) { "invalid command \"#{cmd}\"" }
-      exit 1
-    end
-  end
-
   def dns(argv)
     cmd = argv.shift
     if cmd.nil? then
