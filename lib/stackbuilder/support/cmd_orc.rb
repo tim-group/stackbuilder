42efb003bd5bbb8ad4e48a16baaba7b6dd3ed39f
Tom Denley <tom.denley@timgroup.com>
2018-02-01 18:51:27 +0000

    tomd: tidy up orc_resolve, c.f. original

--- lib/stackbuilder/support/cmd_orc.rb
+++ lib/stackbuilder/support/cmd_orc.rb
@@ -3,18 +3,26 @@ require 'orc/util/option_parser' # XXX decouple orc from stackbuiler somehow
 $LOAD_PATH.delete('/usr/local/lib/site_ruby/timgroup')
 
 module CMDOrc
-  def resolve(_argv)
+  def orc_resolve(_argv)
     machine_def = check_and_get_stack
+    do_orc_resolve(machine_def)
+  end
+
+  def do_orc_resolve(machine_def)
     valid = false
 
     if machine_def.is_a? Stacks::Services::AppServer
       valid = true
       do_resolve(machine_def.virtual_service.application, machine_def.environment.name)
     else
+      applications = Set.new
       machine_def.accept do |child_machine_def|
         next unless child_machine_def.is_a? Stacks::Services::AppServer
         valid = true
-        do_resolve(child_machine_def.virtual_service.application, machine_def.environment.name)
+        applications << child_machine_def.virtual_service.application
+      end
+      applications.to_a.each do |app|
+        do_resolve(app, machine_def.environment.name)
       end
     end
 
