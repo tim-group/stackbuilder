0b045d155fd31d115c236115b011e18801dcb6b1
Tom Denley <tom.denley@timgroup.com>
2018-06-25 11:34:36 +0100

    tomd: pull mcollective hostcleanup support class out of cmd_clean.rb

--- lib/stackbuilder/support/cmd_clean.rb
+++ lib/stackbuilder/support/cmd_clean.rb
@@ -1,12 +1,14 @@
 module CMDClean
-
   def clean_traces(machine_def)
     hosts = []
     machine_def.accept do |child_machine_def|
       hosts << child_machine_def.mgmt_fqdn if child_machine_def.respond_to?(:mgmt_fqdn)
     end
+
+    require 'stackbuilder/support/mcollective_hostcleanup'
+    mcollective_hostcleanup = Support::MCollectiveHostcleanup.new
     %w(nagios mongodb puppet).each do |action|
-      hosts.each { |fqdn| hostcleanup(fqdn, action) }
+      hosts.each { |fqdn| mcollective_hostcleanup.hostcleanup(fqdn, action) }
     end
   end
 
@@ -22,50 +24,6 @@ module CMDClean
       on :unaccounted do |vm|
         logger(Logger::WARN) { "VM was unaccounted for: #{vm}" }
       end
-    end
-  end
-
-  private
-
-  # FIXME: Stolen from hostcleanup application, this does not belong here
-  def status_code(status)
-    return 'OK' if status
-    'ERROR'
-  end
-
-  # FIXME: Stolen from hostcleanup application, this does not belong here
-  def output_result(responses)
-    responses.each do |resp|
-      if resp.results[:statuscode] == 0
-        printf(" %-48s: %s - %s, output: %s\n", resp.results[:sender], \
-               resp.action, \
-               status_code(resp.results[:data][:statuscode]), \
-               resp.results[:data][:status])
-      else
-        printf(" %-48s: %s - ERROR %s\n", resp.results[:sender], resp.action, resp.results[:statusmsg])
-      end
-    end
-  end
-
-  # FIXME: Stolen from hostcleanup application, this does not belong here
-  def hostcleanup(fqdn, action)
-    mco_client('hostcleanup') do |hostcleanup_mc|
-      hostcleanup_mc.progress = false
-      hostcleanup_mc.reset_filter
-      case action
-      when 'puppet'
-        hostcleanup_mc.class_filter('role::puppetserver')
-        hostcleanup_mc.fact_filter 'logicalenv', '/(oy|pg|lon|st)/'
-      when 'mongodb'
-        hostcleanup_mc.class_filter('role::mcollective_registrationdb')
-        hostcleanup_mc.fact_filter 'logicalenv', '/(oy|pg|lon|st)/'
-      when 'nagios'
-        hostcleanup_mc.class_filter('nagios')
-        hostcleanup_mc.fact_filter 'domain', '/(oy|pg|lon)/'
-      when 'metrics'
-        hostcleanup_mc.class_filter('metrics')
-      end
-      output_result hostcleanup_mc.send(action, :fqdn => fqdn)
     end
   end
 end
---
+++ lib/stackbuilder/support/mcollective_hostcleanup.rb
@@ -0,0 +1,47 @@
+require 'stackbuilder/support/namespace'
+require 'stackbuilder/support/mcollective'
+
+class Support::MCollectiveHostcleanup
+  include Support::MCollective
+
+  def hostcleanup(fqdn, action)
+    mco_client('hostcleanup') do |hostcleanup_mc|
+      hostcleanup_mc.progress = false
+      hostcleanup_mc.reset_filter
+      case action
+      when 'puppet'
+        hostcleanup_mc.class_filter('role::puppetserver')
+        hostcleanup_mc.fact_filter 'logicalenv', '/(oy|pg|lon|st)/'
+      when 'mongodb'
+        hostcleanup_mc.class_filter('role::mcollective_registrationdb')
+        hostcleanup_mc.fact_filter 'logicalenv', '/(oy|pg|lon|st)/'
+      when 'nagios'
+        hostcleanup_mc.class_filter('nagios')
+        hostcleanup_mc.fact_filter 'domain', '/(oy|pg|lon)/'
+      when 'metrics'
+        hostcleanup_mc.class_filter('metrics')
+      end
+      output_result hostcleanup_mc.send(action, :fqdn => fqdn)
+    end
+  end
+
+  private
+
+  def status_code(status)
+    return 'OK' if status
+    'ERROR'
+  end
+
+  def output_result(responses)
+    responses.each do |resp|
+      if resp.results[:statuscode] == 0
+        printf(" %-48s: %s - %s, output: %s\n", resp.results[:sender], \
+               resp.action, \
+               status_code(resp.results[:data][:statuscode]), \
+               resp.results[:data][:status])
+      else
+        printf(" %-48s: %s - ERROR %s\n", resp.results[:sender], resp.action, resp.results[:statusmsg])
+      end
+    end
+  end
+end
