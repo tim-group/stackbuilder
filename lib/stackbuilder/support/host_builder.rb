b0d8496e47dc0bea119ed270a9a98e5ccf5763af
Tom Denley <tom.denley@timgroup.com>
2018-06-27 13:52:24 +0100

    tomd: check host is empty

--- lib/stackbuilder/support/host_builder.rb
+++ lib/stackbuilder/support/host_builder.rb
@@ -10,16 +10,15 @@ class Support::HostBuilder
     fabric = hostname.partition('-').first
     host = @factory.host_repository.find_compute_nodes(fabric, false, false, false).hosts.find { |h| h.hostname == hostname }
 
-    bail "unable to find #{hostname}" if host.nil?
+    bail "unable to find #{host_fqdn}" if host.nil?
+    bail "cannot rebuild #{host_fqdn}, it has VMs: #{host.machines.map { |m| m[:hostname] }.join(', ')}" unless host.machines.empty?
 
-    logger(Logger::INFO) { "Will rebuild: #{host.fqdn}" }
-
-    # check host is empty
     # set allocation disabled
     # schedule downtime
     # remove from mco mongodb registry
     # power off
 
+    logger(Logger::INFO) { "Will rebuild #{host.fqdn}" }
     build(host_fqdn)
 
     # unschedule downtime
@@ -36,7 +35,10 @@ class Support::HostBuilder
   def build(host_fqdn)
     # check host exists and is powered off
     # retrieve mac address and setup PXE
+
+    logger(Logger::INFO) { "About to install o/s on #{host_fqdn}" }
     # instigate rebuild
+
     # clean/wait/sign puppet cert
     # sanity check
     # enable allocation
