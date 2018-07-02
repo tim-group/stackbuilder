f89ea142935cbea3c0195df32fbdfffaa8d59bcd
Tom Denley <tom.denley@timgroup.com>
2018-07-02 14:15:28 +0100

    tomd: increase timeout for running all nrpe commands, and do several attempts.

--- lib/stackbuilder/support/mcollective_nrpe.rb
+++ lib/stackbuilder/support/mcollective_nrpe.rb
@@ -4,17 +4,23 @@ require 'stackbuilder/support/mcollective'
 class Support::MCollectiveNrpe
   include Support::MCollective
 
-  def run_all_commands(host_fqdn)
-    mco_results = mco_client("nrpe", :nodes => [host_fqdn]) do |mco|
-      mco.runallcommands.map do |result|
-        fail "failed to perform nrpe checks on #{host_fqdn}: #{result[:statusmsg]}" if result[:statuscode] != 0
-        result[:data][:commands]
+  def run_all_commands(fqdn, attempts = 3)
+    rsps = mco_client("nrpe", :timeout => 30, :nodes => [fqdn]) { |mco| mco.runallcommands }
+    failed = (rsps.size != 1 || rsps[0][:statuscode] != 0 || rsps[0][:data][:commands].nil?)
+
+    if failed
+      return run_all_commands(fqdn, attempts - 1) if attempts > 1
+
+      if rsps.size == 0
+        fail "Got no response from mcollective nrpe.runallcommands request to #{fqdn}"
+      else
+        fail "failed to perform nrpe checks on #{fqdn}: #{rsps[0][:statusmsg]}" unless rsps[0][:statuscode] == 0
       end
     end
-    fail "Got no response from mcollective nrpe.runallcommands request to #{host_fqdn}" if mco_results.empty?
 
-    check_results = mco_results[0]
-    fail "No nrpe checks executed on #{host_fqdn}" if check_results.nil? || check_results.empty?
-    check_results
+    result = rsps[0][:data][:commands]
+    fail "No nrpe checks executed on #{fqdn}" if result.nil? || result.empty?
+
+    result
   end
 end
