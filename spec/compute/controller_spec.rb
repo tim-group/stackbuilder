0592ddf8158be539ae49347b6bcb3b1d718f9382
Andrew Parker <andy.parker@timgroup.com>
2019-07-31 09:17:58 +0100

    Fix test when working from home
    The hostname for my laptop is lowercased from the code but not from
    `hostname`. Downcase in the test to correct for this....I hope this will
    still work elsewhere.

--- spec/compute/controller_spec.rb
+++ spec/compute/controller_spec.rb
@@ -30,7 +30,7 @@ describe Compute::Controller do
       :fabric => "local"
     }]
 
-    localhost = `hostname -f`.chomp
+    localhost = `hostname -f`.chomp.downcase
 
     expect(@compute_controller.allocate(specs)).to eql(localhost => specs)
   end
