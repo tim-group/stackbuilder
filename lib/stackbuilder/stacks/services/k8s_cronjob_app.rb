c75aae06ddd10f42a74094724c828e6dbab931fa
Waseem Taj <waseem.taj@timgroup.com>
2020-08-03 11:18:45 +0100

    wtaj - setting restart policy to 'Never' - this means on failure ehe pod is not removed.

--- lib/stackbuilder/stacks/services/k8s_cronjob_app.rb
+++ lib/stackbuilder/stacks/services/k8s_cronjob_app.rb
@@ -38,7 +38,7 @@ module Stacks::Services::K8sCronJobApp
       generate_init_container_resource(app_resources_name, app_service_labels, app_name, app_version, replicas, used_secrets, config)
 
     # TODO: check with Waz. I'm setting to restart onFailure as Always is not support. I guess this was probably the default
-    resource_built['spec']['jobTemplate']['spec']['template']['spec']['restartPolicy'] = 'OnFailure'
+    resource_built['spec']['jobTemplate']['spec']['template']['spec']['restartPolicy'] = 'Never'
 
     container_resource = generate_container_resource(app_name, app_version, config)
     container_resource.first['ports'] << { "containerPort" => 5000, "name" => "jmx" }
