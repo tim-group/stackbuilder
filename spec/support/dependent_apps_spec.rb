7ba40139c81e42f760932c365256a334314b8b62
gallan <graham.allan@timgroup.com>
2020-09-02 11:14:34 +0100

    gallan: improve test coverage
     - confirms it ignores 'standard_service', since those don't fit stop/start model
     - check dependencies aren't accidentally picked up where they shouldn't be

--- spec/support/dependent_apps_spec.rb
+++ spec/support/dependent_apps_spec.rb
@@ -6,61 +6,83 @@ def new_environment(name, options)
   Stacks::Environment.new(name, options, nil, {}, {}, Stacks::CalculatedDependenciesCache.new)
 end
 
-describe Support::DependentApps do
-  describe_stack 'should do nothing if no apps depend on host' do
-    given do
-      stack 'intertwined_apps' do
-        mysql_cluster 'the-mysql-db'
-      end
-      env 'e1', :primary_site => 'space' do
-        instantiate_stack "intertwined_apps"
-      end
-    end
-    host('e1-the-mysql-db-001.mgmt.space.net.local') do |host|
-      expect(Support::DependentApps.new(host.environment, host.virtual_service).unsafely_stop_commands).to be_empty
-      expect(Support::DependentApps.new(host.environment, host.virtual_service).unsafely_start_commands).to be_empty
-    end
-  end
+def make_dependent_apps(host)
+  Support::DependentApps.new(host.environment, host.virtual_service)
+end
 
+describe Support::DependentApps do
   describe_stack 'should stop or start all dependent apps' do
     given do
-      stack 'intertwined_apps' do
+      stack 'databases' do
         mysql_cluster 'the-mysql-db'
-
+        mysql_cluster 'mysql-db-no-dependencies'
+      end
+      stack 'dependent_apps' do
         standalone_app_service 'the-vm-app' do
           self.application = 'MyApp'
           self.groups = %w(purple)
-          depend_on 'the-mysql-db', 'e1'
+          depend_on 'the-mysql-db'
         end
+
         app_service 'the-k8s-app', :kubernetes => true do
           self.application = 'MyApp'
           self.groups = %w(yellow)
-          depend_on 'the-mysql-db', 'e1'
+          depend_on 'the-mysql-db'
         end
 
+        standard_service 'not-an-app' do
+          depend_on 'the-mysql-db'
+        end
       end
+
       env 'e1', :primary_site => 'space' do
-        instantiate_stack "intertwined_apps"
+        instantiate_stack "databases"
+        instantiate_stack "dependent_apps"
+      end
+      env 'e2', :primary_site => 'earth' do
+        instantiate_stack "databases"
       end
     end
 
     host('e1-the-mysql-db-001.mgmt.space.net.local') do |host|
-      stop_commands = Support::DependentApps.new(host.environment, host.virtual_service).unsafely_stop_commands
-      expect(stop_commands).to eql([
+      dependent_apps = make_dependent_apps(host)
+
+      expect(dependent_apps.unsafely_stop_commands).to eql([
         DependentAppKubectlCommand.new('the-k8s-app-yellow-app', 'e1', 'space'),
         DependentAppMcoCommand.new('e1', 'MyApp', 'purple', 'stop')
       ].to_set)
 
-      start_commands = Support::DependentApps.new(host.environment, host.virtual_service).unsafely_start_commands
-      expect(start_commands).to eql([
+      expect(dependent_apps.unsafely_start_commands).to eql([
         DependentAppMcoCommand.new('e1', 'MyApp', 'purple', 'start'),
         DependentAppStacksApplyCommand.new('e1', 'the-k8s-app')
       ].to_set)
     end
 
-    xhost('e2-the-mysql-db-001.mgmt.earth.net.local') do |host|
-      expect(Support::DependentApps.new(host.environment, host.virtual_service).unsafely_stop_commands).to be_empty
-      expect(Support::DependentApps.new(host.environment, host.virtual_service).unsafely_start_commands).to be_empty
+    host('e2-the-mysql-db-001.mgmt.earth.net.local') do |host|
+      dependent_apps = make_dependent_apps(host)
+      expect(dependent_apps.unsafely_stop_commands).to be_empty
+      expect(dependent_apps.unsafely_start_commands).to be_empty
     end
+
+    host('e1-mysql-db-no-dependencies-001.mgmt.space.net.local') do |host|
+      dependent_apps = make_dependent_apps(host)
+      expect(dependent_apps.unsafely_stop_commands).to be_empty
+      expect(dependent_apps.unsafely_start_commands).to be_empty
+    end
+  end
+
+  it 'constructs system call to stop service via mco' do
+    expect(DependentAppMcoCommand.new('e1', 'MyApp', 'purple', 'stop').executable).
+      to eql('mco service e1-MyApp-purple stop -F logicalenv=e1 -F application=MyApp -F group=purple')
+  end
+
+  it 'constructs system call to start service via mco' do
+    expect(DependentAppMcoCommand.new('e1', 'MyApp', 'purple', 'start').executable).
+      to eql('mco service e1-MyApp-purple start -F logicalenv=e1 -F application=MyApp -F group=purple')
+  end
+
+  it 'constructs system call to scale down a k8s deployment to zero replicas' do
+    expect(DependentAppKubectlCommand.new('the-k8s-app-yellow-app', 'e1', 'earth').executable).
+      to eql('kubectl --context=earth -n e1 scale deploy the-k8s-app-yellow-app --replicas=0')
   end
 end
